fea5370328d318000e4cf1f1666c4b47ba8d84d9
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-01 15:35:18 -0800

    Create L_344.java

---
+++ leetcode/L_344.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public String reverseString(String s) {
+        byte[] bytes = s.getBytes();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            byte temp = bytes[i];
+            bytes[i] = bytes[j];
+            bytes[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(bytes);
+    }
+}
+
+//https://leetcode.com/problems/reverse-string/description/
