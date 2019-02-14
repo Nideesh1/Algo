41ddee1cd6e10f7b7b5f2a353b64ec3dabca177e
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-13 16:35:57 -0800

    Create L_766.java

---
+++ leetcode/L_766.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int r = 0; r < matrix.length; ++r)
+            for (int c = 0; c < matrix[0].length; ++c)
+                if (r > 0 && c > 0 && matrix[r-1][c-1] != matrix[r][c])
+                    return false;
+        return true;
+    }
+}
+
+//https://leetcode.com/problems/toeplitz-matrix/description/
