687027f63de14fb1607bd2eddb40a409d4fd4a06
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-14 15:57:27 -0800

    Create L_747.java

---
+++ leetcode/L_747.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int maxIndex = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > nums[maxIndex])
+                maxIndex = i;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (maxIndex != i && nums[maxIndex] < 2 * nums[i])
+                return -1;
+        }
+        return maxIndex;
+    }
+}
+
+//https://leetcode.com/problems/largest-number-at-least-twice-of-others/description/
