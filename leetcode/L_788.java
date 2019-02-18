44796fa5ccdac0724db4f10b380d03530930cb5c
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-17 19:46:50 -0800

    Create L_788.java

---
+++ leetcode/L_788.java
@@ -0,0 +1,28 @@
+////dp[i]. 0 is invalid, 1 is valid but not good. 2 is good
+
+
+class Solution {
+    public int rotatedDigits(int N) {
+        int[] dp = new int[N + 1];
+        int count = 0;
+        for(int i = 0; i <= N; i++){
+            if(i < 10){
+                if(i == 0 || i == 1 || i == 8) dp[i] = 1;
+                else if(i == 2 || i == 5 || i == 6 || i == 9){
+                    dp[i] = 2;
+                    count++;
+                }
+            } else {
+                int a = dp[i / 10], b = dp[i % 10];
+                if(a == 1 && b == 1) dp[i] = 1;
+                else if(a >= 1 && b >= 1){
+                    dp[i] = 2;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+//https://leetcode.com/problems/rotated-digits/description/
