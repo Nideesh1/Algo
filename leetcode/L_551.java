7ca016b29506a956a013d932901379f1d7746041
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-18 18:08:24 -0800

    Create L_551.java

---
+++ leetcode/L_551.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean checkRecord(String s) {
+        int count=0;
+        for(int i=0;i<s.length() && count<2 ;i++)
+            if(s.charAt(i)=='A')
+                count++;
+        return count<2 && s.indexOf("LLL")<0;
+    }
+}
+
+
+//https://leetcode.com/problems/student-attendance-record-i/description/
