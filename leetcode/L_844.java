cade37006dcafe9b56c50afd9075bd464901098b
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-25 22:07:46 -0800

    Update L_844.java

--- leetcode/L_844.java
+++ leetcode/L_844.java
@@ -1,3 +1,4 @@
+//Stack
 class Solution {
     public boolean backspaceCompare(String S, String T) {
         Stack<Character> stkS = new Stack<>();
@@ -28,5 +29,45 @@ class Solution {
         return true;
     }
 }
+//Two pointer
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1; int skipS = 0;
+        int j = T.length() - 1; int skipT = 0;
+        
+        while(i >= 0 || j >= 0){
+            
+            
+            while(i >= 0){
+                if(S.charAt(i) == '#'){
+                    skipS++; i--;
+                }
+                else if(skipS > 0){
+                    skipS--; i--;
+                }
+                else break;
+            }
+            while(j >= 0){
+                if(T.charAt(j) == '#'){
+                    skipT++; j--;
+                }
+                else if(skipT > 0){
+                    skipT--; j--;
+                }
+                else break;
+            }
+            
+            if( i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j)){
+                return false;
+            }
+            if( (i >= 0) != (j >= 0)){
+                return false;
+            }
+            i--; j--;
+        }
+        return true;
+    }
+}
+
 
 //https://leetcode.com/problems/backspace-string-compare/description/
