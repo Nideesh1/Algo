c3e72f27db517ac5ee6c32ecf97f733bc1a0d62d
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-25 23:14:00 -0800

    Update L_997.java

--- leetcode/L_997.java
+++ leetcode/L_997.java
@@ -1,34 +1,27 @@
 class Solution {
-    public int[] sortedSquares(int[] A) {
-        for(int i = 0; i < A.length; i++){
-            A[i] = A[i] * A[i];
+    public int findJudge(int N, int[][] trust) {
+        if(trust.length == 0){return 1;}
+        Map<Integer,List<Integer>> town = new HashMap<>();
+        for(int[] t : trust){
+            List<Integer> l = town.getOrDefault(t[0], new ArrayList<>());
+            l.add(t[1]);
+            town.put(t[0], l);
         }
-        qsort(A, 0, A.length-1);
-        return A;
-    }
-    
-    public void qsort(int[] A, int i, int j){
-        if(i < j){
-            int partIndex = part(A, i, j);
-            qsort(A, i, partIndex -1);
-            qsort(A, partIndex +1, j);
-        }
-    }
-    
-    public int part(int[] A, int start, int end){
-        int i = start -1; int pivot = A[end];
-        for(int j = start; j < end; j++){
-            
-            if(A[j] < pivot){
-                i++;
-                //swap a[i] and a[j]
-                int t1 = A[i];
-                A[i] = A[j];
-                A[j] = t1;
+        Map<Integer, Integer> trustCount = new HashMap<>();
+        for(List<Integer> l : town.values()){
+            for(Integer i : l){
+                int num = trustCount.getOrDefault(i,0) + 1;
+                trustCount.put(i,num);
             }
         }
-        int t2 = A[i+1]; A[i+1] = A[end]; A[end] = t2;
-        return i+1;
+        System.out.println(trustCount);
+        for(Map.Entry<Integer,Integer> entry : trustCount.entrySet()){
+            if(entry.getValue() == N-1 && !town.containsKey(entry.getKey())){
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 }
-//https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+//https://leetcode.com/problems/find-the-town-judge/description/
