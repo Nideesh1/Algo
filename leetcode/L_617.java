c7d35a4e4c892fec17e289b23230a503073580ac
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-25 23:15:51 -0800

    Update L_617.java

--- leetcode/L_617.java
+++ leetcode/L_617.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//Stack
 class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
         if(t1 == null) return t2;
@@ -40,5 +41,29 @@ class Solution {
         
     }
 }
+
+//Recur
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
+
 
 //https://leetcode.com/problems/merge-two-binary-trees/description/
