4f4f7352859891f7ee35ae8cf4634208ed5de870
Nideesh Terapalli <nterapalli1@gmail.com>
2019-02-27 10:44:52 -0800

    Create L_538.java

---
+++ leetcode/L_538.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root == null) return null;
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
+
