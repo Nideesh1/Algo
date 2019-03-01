245b35512877f3b92c238d123b6b69a70137b928
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-01 14:53:34 -0800

    Create L_575.java

---
+++ leetcode/L_575.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int distributeCandies(int[] candies) {
+        HashSet < Integer > set = new HashSet < > ();
+        for (int candy: candies) {
+            set.add(candy);
+        }
+        return Math.min(set.size(), candies.length / 2);
+    }
+}
+
+
+//https://leetcode.com/problems/distribute-candies/
