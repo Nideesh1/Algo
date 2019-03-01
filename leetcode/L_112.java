4ea5fd96bbb4a0e35702e8859ccc2511342ab0c7
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-01 01:11:46 -0800

    Create L_112.java

---
+++ leetcode/L_112.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        List<String> paths = new ArrayList<>();
+        recur(root, paths, "");
+        for(String s : paths){
+            if(equals(s, sum))return true;
+        }
+        return false;
+    }
+    
+    public boolean equals(String path, int sum){
+        String[] nums = path.split("->");
+        int[] ar = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            ar[i] = Integer.parseInt(nums[i]);
+        }
+        for(Integer i : ar){ sum -= i;}
+        return sum == 0;
+    }
+    
+    public void recur(TreeNode root, List<String> paths, String path){
+        if(root == null) {
+            return;
+        }
+        path += Integer.toString(root.val);
+        if(root.left == null && root.right == null){
+            paths.add(path);
+        }else{
+            path += "->";
+            recur(root.left, paths, path);
+            recur(root.right, paths, path);
+        }
+        
+        
+        
+    }
+}
+
+//Recur
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        sum -= root.val;
+        if(root.left == null && root.right == null){
+            return sum == 0;
+        }
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}
