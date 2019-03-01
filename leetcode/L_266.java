5e61386d036460be3f1d1556fba9ca9360d35669
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-01 14:41:59 -0800

    Create L_266.java

---
+++ leetcode/L_266.java
@@ -0,0 +1,16 @@
+public class Solution {
+ public boolean canPermutePalindrome(String s) {
+     HashMap < Character, Integer > map = new HashMap < > ();
+     for (int i = 0; i < s.length(); i++) {
+         map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+     }
+     int count = 0;
+    for(char key : map.keySet()){
+        count += map.get(key) % 2;
+    }
+    return count <= 1;
+ }
+}
+
+
+//https://leetcode.com/problems/palindrome-permutation/
