5955eb32dadbc70876ac9b72a7ce3db3cbf4bfb6
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-09 19:31:09 -0800

    Create L_359.java

---
+++ leetcode/L_359.java
@@ -0,0 +1,30 @@
+class Logger {
+
+    Map<String,Integer> map;
+    /** Initialize your data structure here. */
+    public Logger() {
+        map = new HashMap<>(); 
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!map.containsKey(message)){
+            map.put(message,timestamp); return true;
+        }
+            int last = map.get(message);
+            if(timestamp - last >= 10){
+                map.put(message, timestamp); return true;
+            }    
+        return false;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
+ 
+ //https://leetcode.com/problems/logger-rate-limiter/
