a35b4d03f38fb0a558afa09247ea391b9ec9df98
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-17 01:22:14 -0700

    Create L_1013.java

---
+++ leetcode/L_1013.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        int res  = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < time.length; i++){
+            int t = time[i] % 60;
+            int dif = 60- t;
+            if(dif == 60) 
+                dif = 0;
+
+                        
+            if(map.containsKey(t)){
+               res += map.get(t); 
+            }
+            if (!map.containsKey(dif)){
+                map.put(dif, 1);
+            } else{
+                map.put(dif, map.get(dif) + 1);
+            }
+        }
+        return res;
+    }
+}
+
+//https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
