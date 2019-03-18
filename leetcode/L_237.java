227ba72c6f29b11d6bb1aa5dfb5bcbe86b02afe8
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-18 01:37:18 -0700

    Create L_237.java

---
+++ leetcode/L_237.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+//https://leetcode.com/problems/delete-node-in-a-linked-list/
