0d56564f365f3ea64b1e27db14fff084ceb3f9b7
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-27 09:21:09 -0700

    Create L_703.java

---
+++ leetcode/L_703.java
@@ -0,0 +1,24 @@
+   class KthLargest {
+        final PriorityQueue<Integer> q;
+        final int k;
+
+        public KthLargest(int k, int[] a) {
+            this.k = k;
+            //Ensure the PQ is only size K, and automatically removes the smallest and adds
+            q = new PriorityQueue<>(k);
+            for (int n : a)
+                add(n);
+        }
+
+        public int add(int n) {
+            if (q.size() < k)
+                q.offer(n);
+            else if (q.peek() < n) {
+                q.poll();
+                q.offer(n);
+            }
+            return q.peek();
+        }
+    }
+    
+    //https://leetcode.com/problems/kth-largest-element-in-a-stream/
