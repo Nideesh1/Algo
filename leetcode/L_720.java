45c7d89dd5076d84e71e6d2af04ccf3f83ea1a0d
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-29 16:07:29 -0700

    Update L_720.java

--- leetcode/L_720.java
+++ leetcode/L_720.java
@@ -1,3 +1,61 @@
+//Trie Solution
+class Solution {
+    public String longestWord(String[] words) {
+        Trie t = new Trie();
+        int index = 1;
+        for(String s : words){
+            t.insert(s, index++);
+        }
+        t.words = words;        
+        return t.dfs();
+    }
+}
+
+class Node {
+    char c; int index = 0;
+    Map<Character,Node> map = new HashMap<>();
+    Node(char c){
+        this.c = c;
+    }
+}
+class Trie {
+    Node root = new Node('0');
+    String[] words;
+    
+    public void insert(String s, int index){
+        Node cur = root;
+        for(char c : s.toCharArray()){
+            cur.map.putIfAbsent(c, new Node(c));
+            cur = cur.map.get(c);
+        }
+        cur.index = index;
+    }
+    
+    public String dfs(){
+        String ans = "";
+        Stack<Node> stk = new Stack<>();
+        stk.push(root);
+        while(!stk.isEmpty()){
+            Node n = stk.pop();
+            if(n == root || n.index > 0){
+                if(n != root){
+                    String word = words[n.index - 1];
+                    if(word.length() > ans.length() || (word.length() == ans.length() && word.compareTo(ans) < 0)){
+                        ans = word;
+                    }
+                }
+                            for(Node kid : n.map.values()){
+                stk.push(kid);
+                }
+            }
+
+            }
+            return ans;
+        }
+                       
+                 
+}
+
 class Solution {
     public String longestWord(String[] words) {
         List<String> l = new ArrayList<>();
