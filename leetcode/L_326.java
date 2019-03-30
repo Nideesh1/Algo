cf01919ba9c240ec02ca670b7d615c0511a6a550
Nideesh Terapalli <nterapalli1@gmail.com>
2019-03-30 00:30:40 -0700

    Create L_326.java

---
+++ leetcode/L_326.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n < 1) return false;
+        
+        while(n % 3 == 0){
+            n /= 3;
+        }
+        
+        return n == 1;
+    }
+}
+
+//https://leetcode.com/problems/power-of-three/
