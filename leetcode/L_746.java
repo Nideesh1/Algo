01042796f3b39949f08a3dab749d3a4845e53b7a
Nideesh Terapalli <nterapalli1@gmail.com>
2019-04-01 09:25:42 -0700

    Create L_746.java

---
+++ leetcode/L_746.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) { int n = cost.length;
+        //dp[i] is cost ending till i 
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i = 2; i < n; i++){
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+}
+
+//https://leetcode.com/problems/min-cost-climbing-stairs/
