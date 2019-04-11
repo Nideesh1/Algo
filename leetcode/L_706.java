fd30f1ca5eb0b091370af31cc06280ea70767adc
Nideesh Terapalli <nterapalli1@gmail.com>
2019-04-10 23:00:15 -0700

    Update L_706.java

--- leetcode/L_706.java
+++ leetcode/L_706.java
@@ -1,3 +1,98 @@
+//Recollision
+class MyHashMap {
+    
+	/** Store the Bucket's start nodes in the the arraylist. */
+    List<HashNode> buckets;
+    int numberOfBuckets;
+        
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        numberOfBuckets = 10000;
+        buckets = new ArrayList<HashNode>();
+        
+        for(int i=0;i<numberOfBuckets;i++) 
+            buckets.add(null);
+    }
+    
+	/**
+	* Helper method to get the bucket index for remove, put and get methods. 
+	*/
+    public int getBucketIndex(int key) {
+        return Integer.hashCode(key) % numberOfBuckets;
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        int bucketIndex = getBucketIndex(key);
+        HashNode node = buckets.get(bucketIndex);
+        while(node != null) {
+            if (node.key == key) {
+                node.value = value;
+                return;
+            }
+            
+            node = node.next;
+        }
+
+        node = buckets.get(bucketIndex);
+        HashNode newNode = new HashNode(key, value);
+        newNode.next = node;
+        buckets.set(bucketIndex, newNode);
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        int bucketIndex = getBucketIndex(key);
+        HashNode node = buckets.get(bucketIndex);
+        while (node != null) {
+            if (node.key == key) {
+                return node.value;
+            }
+            node = node.next;
+        }
+        
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        int bucketIndex = getBucketIndex(key);
+        HashNode node = buckets.get(bucketIndex);
+        HashNode prev = null;
+        while (node != null) {
+            if(node.key == key) {
+                break;
+            }
+            
+            prev = node;
+            node = node.next;
+        }
+        
+        if (node == null) return ;
+        
+        if (prev != null) {
+            prev.next = node.next;
+        } else {
+            buckets.set(bucketIndex, node.next);
+        }
+    }
+}
+
+/**
+* Class to represent the single hash node. 
+*/
+class HashNode {
+    int key;
+    int value;
+    
+    HashNode next;
+    
+    HashNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
 class MyHashMap {
     
     Node[] arr = new Node[10000];
