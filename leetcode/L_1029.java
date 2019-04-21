258854a7e2534f18dc3e47f4ee1af2f7580ab850
Nideesh Terapalli <nterapalli1@gmail.com>
2019-04-21 15:23:47 -0700

    Update L_1029.java

--- leetcode/L_1029.java
+++ leetcode/L_1029.java
@@ -1,23 +1,42 @@
 class Solution {
-    public List<Boolean> prefixesDivBy5(int[] A) {
-        List<Boolean> b = new ArrayList<>();
-        int start = A[0];
-            if(start % 5 == 0) {b.add(true);}
-            if(start % 5 != 0) {b.add(false);}
-        for(int i = 1; i < A.length; i++){
-            if(A[i] == 1){
-                start = start * 2 + 1;
-            }else{
-                start = start * 2;
+    public int twoCitySchedCost(int[][] costs) {
+        Comparator co = new Comparator<int[]>() {
+            public int compare(int[] a, int[]b){
+                return Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1]);
             }
-            if(start % 5 == 0) {b.add(true);}
-            if(start % 5 != 0) {b.add(false);}
-            start = start % 5;
-        }
-        return b;
-
+        };
+        int n  = costs.length/2;
+        int res = 0;
+        int countA = 0; int countB = 0;
+        Arrays.sort(costs, co);
         
+        for(int i = 0; i < costs.length; i++){
+            for(int j = 0; j < costs[0].length; j++){
+                System.out.print(costs[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        for(int i = 0; i < costs.length; i++){
+            if(countA < n && countB < n){
+                if(costs[i][0] < costs[i][1]){
+                    countA++; res += costs[i][0];
+                }
+                if(costs[i][0] >= costs[i][1]){
+                    countB++; res += costs[i][1];
+                }                
+            }
+            else if(countA < n){
+                countA++; res += costs[i][0];
+            }
+            else if(countB < n){
+                countB++; res += costs[i][1];
+            }
+        }
+        
+        return res;
     }
 }
 
-//https://leetcode.com/problems/binary-prefix-divisible-by-5/
+//Credit To:https://leetcode.com/problems/two-city-scheduling/discuss/278979/Concise-Java-solution-Time%3A-O(nlogn)
+//Link:https://leetcode.com/problems/two-city-scheduling/
