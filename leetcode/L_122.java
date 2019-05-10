1fd21e4ccde1fd04ab50469d990d5b2d1e66cfce
Nideesh Terapalli <nterapalli1@gmail.com>
2019-05-10 04:35:39 -0700

    Update L_122.java

--- leetcode/L_122.java
+++ leetcode/L_122.java
@@ -1,3 +1,17 @@
+//DP
+class Solution {
+    public int maxProfit(int[] prices) {
+        int T_ik0 = 0;
+        int T_ik1 = Integer.MIN_VALUE;
+        
+        for(int i : prices){
+            T_ik0 = Math.max(T_ik0, T_ik1 + i);
+            T_ik1 = Math.max(T_ik1, T_ik0 - i);
+        }
+        return T_ik0;
+    }
+}
+
 class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
@@ -20,3 +34,4 @@ class Solution {
 }
 
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/discuss/108870/Most-consistent-ways-of-dealing-with-the-series-of-stock-problems
