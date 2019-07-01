09003e10426312596f56b2bc3474adab53b60639
Nideesh Terapalli <nterapalli1@gmail.com>
2019-07-01 01:32:47 -0700

    Update L_684.java
    update

--- leetcode/L_684.java
+++ leetcode/L_684.java
@@ -57,3 +57,45 @@ class Subset{
 
 //https://leetcode.com/problems/redundant-connection/
 //https://leetcode.com/problems/redundant-connection/solution/
+
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length + 1;
+        UF uf = new UF(n);
+              
+        for(int[] e : edges){
+            if(!uf.union(e[0], e[1]))return e;
+        }
+
+        return null;
+    }
+}
+
+class UF{
+    int[] sub;
+    public UF(int size){
+        sub = new int[size];
+        for(int i = 0; i < size; i++){
+            sub[i] = i;
+
+        }
+    }
+    int find(int x){
+        if(x != sub[x]){
+            sub[x] = find(sub[x]);
+        }
+        return sub[x];
+    }
+    boolean union (int x, int y){
+        int xr = find(x); int yr = find(y);
+        
+        //if they are already part of same parent return false, they don't need to 
+        //combine
+        if(xr == yr)
+        return false;
+        
+        sub[xr] = find(yr);
+        return true;
+    }
+}
