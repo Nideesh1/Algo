41fdc38ab12ca9155ae0a08f237005219b51872e
Nideesh Terapalli <nterapalli1@gmail.com>
2019-09-06 00:39:16 -0700

    Update L_205.java

--- leetcode/L_205.java
+++ leetcode/L_205.java
@@ -28,4 +28,18 @@ class Solution {
     }
 }
 
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Integer> m1 = new HashMap<>();
+        Map<Character,Integer> m2 = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!m1.containsKey(s.charAt(i))) m1.put(s.charAt(i), i);
+            if(!m2.containsKey(t.charAt(i))) m2.put(t.charAt(i), i);
+            if(m1.get(s.charAt(i)) != m2.get(t.charAt(i))) return false;
+        }
+        System.out.println(m1);         System.out.println(m2);
+        return true;
+    }
+}
+
 //https://leetcode.com/problems/isomorphic-strings/
