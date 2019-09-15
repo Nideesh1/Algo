d2bdf94113877adb6d033632864749ab1ef3285f
Nideesh Terapalli <nterapalli1@gmail.com>
2019-09-15 11:10:51 -0700

    Create L_18.java

---
+++ leetcode/L_18.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, 0, 4, target);
+    }
+    public List<List<Integer>> kSum(int[] nums, int start, int k , int target){
+        Set<List<Integer>> res = new HashSet<>();
+        if(k == 2){
+            int lo = start; int hi = nums.length - 1;
+            while(lo < hi){
+                int sum = nums[lo] + nums[hi];
+                if(sum == target){
+                    List<Integer> l = new ArrayList<>();
+                    l.add(nums[lo]); l.add(nums[hi]);
+                    res.add(l);
+                    lo++; hi--;
+                }
+                if(sum < target){
+                    lo++;
+                }if(sum > target){
+                    hi--;
+                }
+            }
+        }else{
+            for(int i = start; i < nums.length - (k-1); i++){
+                if(i > start && nums[i] == nums[i-1]) continue;
+                List<List<Integer>> temp = kSum(nums, i+1, k-1, target - nums[i]);
+                for(List<Integer> t : temp){
+                    t.add(0, nums[i]);
+                }
+                res.addAll(temp);
+            }
+        }
+        return new ArrayList<>(res);
+    }
+}
+
+//https://leetcode.com/problems/4sum/
