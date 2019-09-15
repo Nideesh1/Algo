e9634b45daea51205d165c576be193bbfc177592
Nideesh Terapalli <nterapalli1@gmail.com>
2019-09-15 13:59:58 -0700

    Update L_116.java

--- leetcode/L_116.java
+++ leetcode/L_116.java
@@ -55,4 +55,40 @@ class Solution {
     }
 }
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        Node level_start = root;
+        Node cur = root;
+        while(level_start != null){
+            cur = level_start;
+            while(cur != null){
+                if(cur.left != null) cur.left.next = cur.right;
+                if(cur.right != null && cur.next != null) cur.right.next = cur.next.left;
+                
+                cur = cur.next;
+            }
+            level_start = level_start.left;
+        }
+        return root;
+    }
+}
+
 //https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
