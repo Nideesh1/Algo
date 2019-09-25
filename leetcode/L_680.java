98fc88d1ccefb55388ab2fa220880154e288cdd4
Nideesh Terapalli <nterapalli1@gmail.com>
2019-09-24 22:47:17 -0700

    Update L_680.java

--- leetcode/L_680.java
+++ leetcode/L_680.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        for(int i = 0; i < s.length()/2; i++){
-            
-            if(s.charAt(i) != s.charAt(s.length() - 1 - i)){
-                int j = s.length() - 1- i;
-                return palindromeRange(s, i, j-1) || palindromeRange(s, i+1, j);
+        int count = 0;
+        int j = 0;
+        for(int i = 0; i < s.length() / 2; i++){
+            j = s.length() - 1 - i;
+            if(s.charAt(i) != s.charAt(j)){
+                return rem(s, i+1, j) || rem(s, i, j - 1);
             }
-            
         }
         return true;
     }
     
-    public boolean palindromeRange(String s, int i , int j){
-        for(int k = i; k <= i + (j-i)/2; k++){
-            if(s.charAt(k) != s.charAt(j + i - k)){
-                return false;
-            }
+    public boolean rem(String s, int i, int j){
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++; j--;
         }
         return true;
     }
