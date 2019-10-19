00069458019e07af69fd5a93a795a96523278176
Nideesh Terapalli <nterapalli1@gmail.com>
2019-10-19 09:36:03 -0700

    Update L_207.java

--- leetcode/L_207.java
+++ leetcode/L_207.java
@@ -37,4 +37,36 @@ class Solution {
     }
 }
 
+class Solution {
+    int n;
+    int[] indegree;
+    Map<Integer,List<Integer>> adj = new HashMap<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        n = numCourses;
+        indegree = new int[n];
+        for(int[] pr : prerequisites){
+            List<Integer> l =  adj.getOrDefault(pr[1], new ArrayList<>());
+            l.add(pr[0]); indegree[pr[0]]++;
+            adj.put(pr[1], l);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
+        for(int i = 0; i < indegree.length; i++){
+            if(indegree[i] == 0) q.add(i); 
+        }
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            if(indegree[cur] == 0)count++;
+            if(!adj.containsKey(cur)) continue;
+            for(int nei : adj.get(cur)){
+                indegree[nei]--;
+                if(indegree[nei]== 0) q.add(nei);
+            }
+
+        }
+
+        return count == n;
+    }
+}
+
 //https://leetcode.com/problems/course-schedule/
