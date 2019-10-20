6ceca4b1dca359f919f47c9709e8b01a554c07e2
Nideesh Terapalli <nterapalli1@gmail.com>
2019-10-20 16:08:08 -0700

    Update L_953.java

--- leetcode/L_953.java
+++ leetcode/L_953.java
@@ -1,28 +1,33 @@
 class Solution {
+    int[] dict = new int[26];
     public boolean isAlienSorted(String[] words, String order) {
-        int[] rank = new int[26];
-        for(int i = 0; i < rank.length; i++){
-            rank[order.charAt(i) - 'a'] = i;
+        for(int i = 0; i < order.length(); i++){
+            char c = order.charAt(i);
+            dict[c - 'a'] = i;
         }
-        outer:
-        for(int i = 0; i < words.length - 1; i++){
-            String s1 = words[i]; String s2 = words[i+1];
-            
-            for(int j = 0; j < Math.min(s1.length(), s2.length()); j++){
-
-                if(rank[s1.charAt(j) - 'a'] < rank[s2.charAt(j) - 'a']) {
-                    continue outer;
-                }else{
-                    return false;
-                }
-            }
-            if(s1.length() > s2.length()) {
-
-                return false;
-            }
+        System.out.println(Arrays.toString(dict));
+        if(words.length <= 1) return true;
+        for(int i = 1; i < words.length; i++){
+            if(!order(words[i-1], words[i])) return false;
         }
         return true;
     }
+    
+    public boolean order(String one, String two){
+        boolean same = true;
+        for(int i = 0; i < one.length() && i <  two.length(); i++){
+            char o = one.charAt(i);
+            char t = two.charAt(i);
+            if(o == t){
+                continue;
+            }
+            if(o != t) same = false;
+            
+            if(!same){
+                return dict[o-'a'] < dict[t-'a'] ? true : false;
+            }
+        }
+        if(one.length() > two.length()){ return false;}        
+        return true;
+    }
 }
-
-//https://leetcode.com/problems/verifying-an-alien-dictionary/
