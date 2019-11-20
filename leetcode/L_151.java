f5be519f89da2f7f33303a79d870df1e885ab258
Nideesh Terapalli <nterapalli1@gmail.com>
2019-11-19 23:22:43 -0800

    Update L_151.java

--- leetcode/L_151.java
+++ leetcode/L_151.java
@@ -17,4 +17,53 @@ class Solution {
     }
 }
 
+class Solution {
+    public String reverseWords(String s) {
+        //reverse whole word
+        char[] a = s.toCharArray();
+        rev(a, 0, s.length() - 1);
+        System.out.println(Arrays.toString(a));
+        //rev each word
+        revEach(a);
+        System.out.println(Arrays.toString(a));
+        
+        //clean emptys
+        return clean(a);
+    }
+    public String clean(char[] a){
+        int n = a.length;
+        int i = 0; int j = 0;
+
+        while(j < n){
+            while(j < n && a[j] == ' ')j++;
+            while(j < n && a[j] != ' ')a[i++] = a[j++];
+            while(j < n && a[j] == ' ')j++;
+            if(j < n) a[i++] = ' ';
+        }
+        return new String(a).substring(0, i);
+    }
+    
+    
+    public void revEach(char[] a){
+        int i = 0; int j = 0;
+        while(j <= a.length){
+            if(j == a.length || a[j] == ' '){
+                rev(a, i, j-1);
+                i = j + 1;
+            }
+            j++;
+        }
+    }
+    
+    public void rev(char[] a, int s, int e){
+        while(s <= e){
+            char c = a[s];
+            a[s] = a[e];
+            a[e] = c;
+            s++; e--;
+        }
+    }
+}
+
+
 //https://leetcode.com/problems/reverse-words-in-a-string/
