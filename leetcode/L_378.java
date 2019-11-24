b953e6c0c7310e0d822bea9a41488b43d3ff9aa0
Nideesh Terapalli <nterapalli1@gmail.com>
2019-11-23 20:11:43 -0800

    Update L_378.java

--- leetcode/L_378.java
+++ leetcode/L_378.java
@@ -30,4 +30,32 @@ class Solution {
     }
 }
 
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        
+        int lo = matrix[0][0]; 
+        int hi = matrix[n-1][n-1];
+        
+        while(lo < hi){
+            int mid = lo + (hi - lo)/2;
+            
+            int count = 0; int j = n-1;
+            for(int i = 0; i < n; i++){
+                while(j >= 0 && matrix[i][j] > mid){
+                    j--;
+                }
+                count += j + 1;
+            }
+            
+            if(count < k)lo = mid + 1;
+            else hi = mid;
+        }
+        
+        return lo;
+    }
+}
+
+//https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/301357/Java-0ms-(added-Python-and-C%2B%2B)%3A-Easy-to-understand-solutions-using-Heap-and-Binary-Search
+
 //https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
