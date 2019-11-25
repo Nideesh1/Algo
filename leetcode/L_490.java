365cea02212cce008ab2447d4bbee64500ef5e19
Nideesh Terapalli <nterapalli1@gmail.com>
2019-11-24 22:13:52 -0800

    Create L_490.java

---
+++ leetcode/L_490.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        int[][] dirs={{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        Queue < int[] > queue = new LinkedList < > ();
+        queue.add(start);
+        visited[start[0]][start[1]] = true;
+        while (!queue.isEmpty()) {
+            int[] s = queue.remove();
+            if (s[0] == destination[0] && s[1] == destination[1])
+                return true;
+            for (int[] dir: dirs) {
+                int x = s[0] + dir[0];
+                int y = s[1] + dir[1];
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                }
+                if (!visited[x - dir[0]][y - dir[1]]) {
+                    queue.add(new int[] {x - dir[0], y - dir[1]});
+                    visited[x - dir[0]][y - dir[1]] = true;
+                }
+            }
+        }
+        return false;
+    }
+}
+//https://leetcode.com/problems/the-maze/solution/
