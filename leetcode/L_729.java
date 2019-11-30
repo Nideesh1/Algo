340aa9a07fe546ee08cd64e077b3fca3d325e6b5
Nideesh Terapalli <nterapalli1@gmail.com>
2019-11-29 18:36:45 -0800

    Create L_729.java

---
+++ leetcode/L_729.java
@@ -0,0 +1,20 @@
+class MyCalendar {
+    TreeMap<Integer, Integer> calendar;
+
+    MyCalendar() {
+        calendar = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+        Integer prev = calendar.floorKey(start),
+                next = calendar.ceilingKey(start);
+        if ((prev == null || calendar.get(prev) <= start) &&
+                (next == null || end <= next)) {
+            calendar.put(start, end);
+            return true;
+        }
+        return false;
+    }
+}
+
+//https://leetcode.com/problems/my-calendar-i/solution/
