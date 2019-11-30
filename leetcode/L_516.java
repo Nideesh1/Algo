3f9eebbd40663ccba60a3d8b63232e25d5c191d0
Nideesh Terapalli <nterapalli1@gmail.com>
2019-11-29 17:06:03 -0800

    Create L_516.java

---
+++ leetcode/L_516.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for(int i = n - 1; i >= 0; i--){
+            dp[i][i] = 1;
+            for(int j = i + 1; j < n; j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }else{
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+}
+
+//https://leetcode.com/problems/longest-palindromic-subsequence/
