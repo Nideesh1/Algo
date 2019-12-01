c8f7fd465d4effbd9baf3bbadd48a0db39179528
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-01 03:13:31 -0800

    Create L_96.java

---
+++ leetcode/L_96.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] G = new int[n+1];
+        G[0] = G[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+}
+
+//https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
