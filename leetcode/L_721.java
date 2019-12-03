6c9a07f1d1e2ad2b1771b252b2acf11f71faa21e
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-03 10:16:44 -0800

    Update L_721.java

--- leetcode/L_721.java
+++ leetcode/L_721.java
@@ -1,38 +1,73 @@
 class Solution {
-    public int minimumDeleteSum(String s1, String s2) {
-        int m = s1.length(); int n = s2.length();
-        int[][] dp = new int[m+1][n+1];
-        
-        // s1 down the column, s2 at top
-        //   _       s    e    a
-        // _ 0       s    s,e  s,e,a
-        // e e       
-        // a e,a
-        // t e,a,t
-        
-        for(int k = 1; k <= n; k++){
-            dp[0][k] = dp[0][k-1] + s2.codePointAt(k-1);
-        }
-        for(int k = 1; k <= m; k++){
-            dp[k][0] = dp[k-1][0] + s1.codePointAt(k-1);
-        }  
-        
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                    int left = dp[i][j-1] + s2.charAt(j-1);
-                    int top = dp[i-1][j] + s1.charAt(i-1);
-                    dp[i][j] = Math.min(left,top);
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        UF dsu = new UF(10001);
+        Map<String, String> emailToName = new HashMap();
+        Map<String, Integer> emailToID = new HashMap();
+        int id = 0;
+        for (List<String> account: accounts) {
+            String name = "";
+            for (String email: account) {
+                if (name == "") {
+                    name = email;
+                    continue;
                 }
-                
+                emailToName.put(email, name);
+                if (!emailToID.containsKey(email)) {
+                    emailToID.put(email, id++);
+                }
+                dsu.union(emailToID.get(account.get(1)), emailToID.get(email));
             }
         }
 
-        return dp[m][n];
+        Map<Integer, List<String>> ans = new HashMap();
+        for (String email: emailToName.keySet()) {
+            int index = dsu.find(emailToID.get(email));
+            ans.computeIfAbsent(index, x-> new ArrayList()).add(email);
+        }
+        for (List<String> component: ans.values()) {
+            Collections.sort(component);
+            component.add(0, emailToName.get(component.get(0)));
+        }
+        return new ArrayList(ans.values());
+    }
+}
+class UF{
+    Sub[] ar;
+    UF(int size){
+        ar = new Sub[size];
+        for(int i = 0; i < size; i++){
+            ar[i] = new Sub(i, 0);
+        }
+    }
+    
+    public int find(int x){
+        if(x != ar[x].par){
+            ar[x].par = find(ar[x].par);
+        }
+        return ar[x].par;
+    }
+    
+    public void union(int x, int y){
+        int xp = find(x); int yp = find(y);
+        if(xp != yp){
+            if(ar[xp].rank < ar[yp].rank){
+                ar[xp].par = yp;
+            }
+            else if(ar[xp].rank > ar[yp].rank){
+                ar[yp].par = xp;
+            }else{
+                ar[xp].par = yp;
+                ar[yp].rank++;
+            }
+        }
     }
 }
 
-//https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/
+class Sub{
+    int par;
+    int rank;
+    Sub(int par, int rank){
+        this.par = par;
+        this.rank = rank;
+    }
+}
