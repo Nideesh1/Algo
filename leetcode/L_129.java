890ba91d3a1ac694fb5ed88d532eceb99aecfa6e
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-03 11:47:31 -0800

    Create L_129.java

---
+++ leetcode/L_129.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+public int sumNumbers(TreeNode root) {
+	return sum(root, 0);
+}
+
+public int sum(TreeNode n, int s){
+	if (n == null) return 0;
+	if (n.right == null && n.left == null) return s*10 + n.val;
+	return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+}
+}
+
+//https://leetcode.com/problems/sum-root-to-leaf-numbers/discuss/41363/Short-Java-solution.-Recursion.
