a3ac161b01515e7818071f21760326cedf6dd014
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-03 12:02:26 -0800

    Create L_209.java

---
+++ leetcode/L_209.java
@@ -0,0 +1,20 @@
+class Solution {
+public int minSubArrayLen(int s, int[] a) {
+  if (a == null || a.length == 0)
+    return 0;
+  
+  int i = 0, j = 0, sum = 0, min = Integer.MAX_VALUE;
+  
+  while (j < a.length) {
+    sum += a[j++];
+    while (sum >= s) {
+      min = Math.min(min, j - i);
+      sum -= a[i++];
+    }
+  }
+  
+  return min == Integer.MAX_VALUE ? 0 : min;
+}
+}
+
+//https://leetcode.com/problems/minimum-size-subarray-sum/solution/
