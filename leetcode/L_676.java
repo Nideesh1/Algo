caf9d7158e48607f3f25251e5e8d7b50c4dad6ff
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-03 11:20:53 -0800

    Create L_676.java

---
+++ leetcode/L_676.java
@@ -0,0 +1,41 @@
+public class MagicDictionary {
+    Set<String> words;
+    Map<String, Integer> count;
+
+    public MagicDictionary() {
+        words = new HashSet();
+        count = new HashMap();
+    }
+
+    private ArrayList<String> generalizedNeighbors(String word) {
+        ArrayList<String> ans = new ArrayList();
+        char[] ca = word.toCharArray();
+        for (int i = 0; i < word.length(); ++i) {
+            char letter = ca[i];
+            ca[i] = '*';
+            String magic = new String(ca);
+            ans.add(magic);
+            ca[i] = letter;
+        }
+        return ans;
+    }
+
+    public void buildDict(String[] words) {
+        for (String word: words) {
+            this.words.add(word);
+            for (String nei: generalizedNeighbors(word)) {
+                count.put(nei, count.getOrDefault(nei, 0) + 1);
+            }
+        }
+    }
+
+    public boolean search(String word) {
+        for (String nei: generalizedNeighbors(word)) {
+            int c = count.getOrDefault(nei, 0);
+            if (c > 1 || c == 1 && !words.contains(word)) return true;
+        }
+        return false;
+    }
+}
+
+//https://leetcode.com/problems/implement-magic-dictionary/solution/
