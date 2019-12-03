f70f589371f619324c8179a0b9976ea567f7f36e
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-03 11:55:40 -0800

    Create L_658.java

---
+++ leetcode/L_658.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] A, int k, int x) {
+        int left = 0, right = A.length - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return Arrays.stream(A, left, left + k).boxed().collect(Collectors.toList());
+    }
+}
+
+//https://leetcode.com/problems/find-k-closest-elements/discuss/106426/JavaC%2B%2BPython-Binary-Search-O(log(N-K)-%2B-K)
