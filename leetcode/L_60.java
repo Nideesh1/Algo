45b23d3959a658d4ad488ac5b402069e722d9158
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-04 11:04:17 -0800

    Update L_60.java

--- leetcode/L_60.java
+++ leetcode/L_60.java
@@ -1,33 +1,29 @@
 class Solution {
     public String getPermutation(int n, int k) {
-   LinkedList<Integer> notUsed = new LinkedList<Integer>();
-
-	int weight = 1;
-
-	for (int i = 1; i <= n; i++) {
-		notUsed.add(i);
-		if (i == n)
-			break;
-		weight = weight * i;
-	}
-
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> num = new ArrayList<>();
         
-	String res = "";
-	k = k - 1;
-        System.out.println(notUsed);
-        System.out.println(weight);
-        System.out.println(k);
-        while (true) {
-		res = res + notUsed.remove(k / weight);
-		k = k % weight;
-		if (notUsed.isEmpty())
-			break;
-		weight = weight / notUsed.size();
-	}
-
-	return res;
+        int fact = 1;
         
+        for(int i = 1; i <= n; i++){
+            fact *= i;
+            num.add(i);
+        }
+        
+        System.out.println(fact);
+        
+        for(int i = 0, l = k - 1; i < n; i++){
+            fact /= (n-i);
+            int idx = (l / fact);
+            
+            sb.append(num.remove(idx));
+            l -= idx * fact;
+        }
+        
+        
+        return sb.toString();
     }
 }
 
-//https://leetcode.com/problems/permutation-sequence/discuss/22665/Clean-Java-Solution
+
+//https://leetcode.com/problems/permutation-sequence/discuss/22507/%22Explain-like-I'm-five%22-Java-Solution-in-O(n)
