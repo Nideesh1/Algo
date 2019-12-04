ca7079773775607777434477e43a712d1c4ca105
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-04 14:16:20 -0800

    Create L_221.java

---
+++ leetcode/L_221.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maxsqlen = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
+                }
+            }
+        }
+        
+        
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+             
+                System.out.print(dp[i][j] + " ");
+                
+            }
+            System.out.println();
+        }
+        
+        return maxsqlen * maxsqlen;
+    }
+}
+
+//https://leetcode.com/problems/maximal-square/solution/
