39137a8df447603d443bda2f3b912f561165f475
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-04 21:46:12 -0800

    Create L_427.java

---
+++ leetcode/L_427.java
@@ -0,0 +1,28 @@
+class Solution {
+    public Node construct(int[][] grid) {
+        return helper(grid, 0, 0, grid.length);
+    }
+    private Node helper(int[][] grid, int x, int y, int len) {
+        if (len == 1) {
+            return new Node(grid[x][y] != 0, true, null, null, null, null);
+        }
+        Node result = new Node();
+        Node topLeft = helper(grid, x, y, len / 2);
+        Node topRight = helper(grid, x, y + len / 2, len / 2);
+        Node bottomLeft = helper(grid, x + len / 2, y, len / 2);
+        Node bottomRight = helper(grid, x + len / 2, y + len / 2, len / 2);
+        if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf
+           && topLeft.val == topRight.val && topRight.val == bottomLeft.val && bottomLeft.val == bottomRight.val) {
+            result.isLeaf = true;
+            result.val = topLeft.val;
+        } else {
+            result.topLeft = topLeft;
+            result.topRight = topRight;
+            result.bottomLeft = bottomLeft;
+            result.bottomRight = bottomRight;
+        }
+        return result;
+    }
+}
+
+//https://leetcode.com/problems/construct-quad-tree/discuss/154565/Java-recursive-solution
