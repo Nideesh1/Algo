d39b0234b17d0bbade74bb794a097d97a8bd04c3
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-21 17:04:15 -0800

    Update L_1002.java

--- leetcode/L_1002.java
+++ leetcode/L_1002.java
@@ -1,4 +1,27 @@
 import java.util.concurrent.ConcurrentHashMap;
+//https://leetcode.com/problems/find-common-characters/discuss/259707/Java-Clean-Solution
+class Solution {
+    public List<String> commonChars(String[] A) {
+        List<String> result = new ArrayList<>();
+        
+        for (char c = 'a'; c <= 'z'; c++){
+            int minCount = Integer.MAX_VALUE;
+            for (String s : A){
+                int wordCount = 0;
+                for (char cur_c : s.toCharArray()) if (cur_c == c) wordCount++;
+                minCount = Math.min(minCount, wordCount);
+            }
+            
+            for (int i = 0; i < minCount; i++) result.add("" + c);
+        }
+        
+        return result;
+}
+    
+    
+}
+
+
 
 class Solution {
     public List<String> commonChars(String[] A) {
