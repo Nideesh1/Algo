78b919567dab66c18ab9dff8705c1a6a0a632867
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-22 18:51:24 -0800

    Update L_105.java

--- leetcode/L_105.java
+++ leetcode/L_105.java
@@ -6,44 +6,39 @@
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
  */
 class Solution {
+    
     int pre_idx = 0;
     int[] pre;
     int[] in;
     
-    Map<Integer,Integer> map = new HashMap<>();
+    Map<Integer,Integer> inMap = new HashMap<>();
+    
+    
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int idx = 0;
         this.pre = preorder;
         this.in = inorder;
         for(Integer i : in){
-            map.put(i, idx++);
+            inMap.put(i, idx++);
         }
         return dfs(0, pre.length - 1);
     }
     
-    public TreeNode dfs(int l, int r){
-        if(l > r){
+    public TreeNode dfs(int i, int j){
+        if(i > j){
             return null;
         }
         
         int rval = pre[pre_idx];
-        int index = map.get(rval);
+        int index = inMap.get(rval);
         
         TreeNode root = new TreeNode(rval);
         
         pre_idx++;
-        root.left = dfs(l, index - 1);
-        root.right = dfs(index+1, r);
+        root.left = dfs(i, index - 1);
+        root.right = dfs(index + 1, j);
         
         return root;
     }
