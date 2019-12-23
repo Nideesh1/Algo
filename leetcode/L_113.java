0dff96ba9d0230ddc63f3c2815923e3bc020232e
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-22 20:09:15 -0800

    Update L_113.java

--- leetcode/L_113.java
+++ leetcode/L_113.java
@@ -7,6 +7,25 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList();
+        if (root == null) return res;
+        dfs(root, sum, res, new ArrayList());
+        return res;
+    }
+    private void dfs(TreeNode root, int target, List<List<Integer>> res, List<Integer> list) {
+        if (root == null) return;
+        list.add(root.val);
+        if (root.left == null && root.right == null && target == root.val)
+            res.add(new ArrayList(list));
+        dfs(root.left, target - root.val, res, list);
+        dfs(root.right, target - root.val, res, list);
+        list.remove(list.size() - 1);
+    }
+}
+
+
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> res = new ArrayList<>();
