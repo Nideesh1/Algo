bdec305306abb26c5716de4af6fe72be3c7e3568
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-27 15:19:30 -0800

    Update L_189.java

--- leetcode/L_189.java
+++ leetcode/L_189.java
@@ -17,4 +17,22 @@ class Solution {
     }
 }
 
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        rev(nums, 0, nums.length - 1);
+        rev(nums, 0, k-1);
+        rev(nums, k, nums.length - 1);
+    }
+    
+    public void rev(int[] ar, int i , int j){
+        while(i < j){
+            int t = ar[i];
+            ar[i] = ar[j];
+            ar[j] = t;
+            i++; j--;
+        }
+    }
+}
+
 //https://leetcode.com/problems/rotate-array/description/
