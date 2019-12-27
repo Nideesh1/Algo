393270694ad7c6bb491e07374ff38115a7174a5c
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-27 15:31:33 -0800

    Update L_190.java

--- leetcode/L_190.java
+++ leetcode/L_190.java
@@ -1,5 +1,18 @@
 public class Solution {
     // you need treat n as an unsigned value
+     public int reverseBits(int n) {
+            int ret=n;
+            ret = ret >>> 16 | ret<<16;
+            ret = (ret & 0xff00ff00) >>> 8 | (ret & 0x00ff00ff) << 8;
+            ret = (ret & 0xf0f0f0f0) >>> 4 | (ret & 0x0f0f0f0f) << 4;
+            ret = (ret & 0xcccccccc) >>> 2 | (ret & 0x33333333) << 2;
+            ret = (ret & 0xaaaaaaaa) >>> 1 | (ret & 0x55555555) << 1;
+            return ret;
+    }
+}
+
+public class Solution {
+    // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int res = 0;
         List<Integer> l = new ArrayList<>();
