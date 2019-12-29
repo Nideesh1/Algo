19cd20954c9f9b468ca4efdb8600a6a9053dc038
Nideesh Terapalli <nterapalli1@gmail.com>
2019-12-28 18:31:17 -0800

    Update L_219.java

--- leetcode/L_219.java
+++ leetcode/L_219.java
@@ -1,15 +1,14 @@
 class Solution {
-public boolean containsNearbyDuplicate(int[] nums, int k) {
-    Set<Integer> set = new TreeSet<>();
-    for (int i = 0; i < nums.length; ++i) {
-        if (set.contains(nums[i])) return true;
-        set.add(nums[i]);
-        if (set.size() > k) {
-            set.remove(nums[i - k]);
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if(set.size() > k){
+                set.remove(nums[i- k]);
+            }
         }
+        return false;
     }
-    return false;
 }
-}
-
 //https://leetcode.com/problems/contains-duplicate-ii/description/
