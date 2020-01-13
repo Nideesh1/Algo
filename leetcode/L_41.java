dc079dfb36643dd758ea757487544d7128223e53
Nideesh Terapalli <nterapalli1@gmail.com>
2020-01-13 08:23:52 -0800

    Update L_41.java

--- leetcode/L_41.java
+++ leetcode/L_41.java
@@ -1,31 +1,37 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        boolean firstExists = false;
-        for(int i : nums){
-            if(i == 1) firstExists = true;
+        boolean oneExist = false;
+        for(int o : nums){
+            if(o == 1) oneExist = true;
         }
-        if(!firstExists) return 1;
-        if(n == 1) return 2;
+        if(!oneExist) return 1;
         
+        //making sure we will never see a number in the 
+        //array apart from 1...n
         for(int i = 0; i < nums.length; i++){
             if(nums[i] <= 0 || nums[i] > n) nums[i] = 1;
         }
         
         for(int i = 0; i < nums.length; i++){
             int v = Math.abs(nums[i]);
-            //So we don't go out of bounds
+            
+            
+            //simply invalidating an index v and it's content
+            //because we found a value v
             if(v == n){
                 nums[0] = -1 * Math.abs(nums[0]);
             }else{
-                nums[v] = -1 * Math.abs(nums[v]);
+                nums[v] = -1* Math.abs(nums[v]);
             }
         }
-        for(int i = 1; i < nums.length; i++){
+        
+        for(int i = 1; i < n; i++){
             if(nums[i] > 0) return i;
         }
+        
         if(nums[0] > 0) return n;
-        return n+1;
+        return n + 1;
     }
 }
 
