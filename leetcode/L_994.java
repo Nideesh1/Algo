a6585d4fc89b83cdb16c8463b53c1e255470046f
Nideesh Terapalli <nterapalli1@gmail.com>
2020-01-19 17:29:01 -0800

    Update L_994.java

--- leetcode/L_994.java
+++ leetcode/L_994.java
@@ -10,13 +10,12 @@ class O {
 class Solution {
     public int orangesRotting(int[][] grid) {
         int R = grid.length; int C = grid[0].length;
-        boolean[][] visited = new boolean[R][C];
+        //boolean[][] visited = new boolean[R][C];
         Queue<O> q = new ArrayDeque<>();
         for(int i = 0; i < R; i++){
             for(int j = 0; j < C; j++){
                 if(grid[i][j] == 2){
                     q.add(new O(i, j, 0));
-                    visited[i][j] = true;
                 }
             }
         }
@@ -24,38 +23,21 @@ class Solution {
         int res = 0;
         
         while(!q.isEmpty()){
-            O o = q.poll();
+            O cur = q.poll();
 
-            res = Math.max(res, o.layer);
+            res = Math.max(res, cur.layer);
             
-            //up
-            if( inside(grid,o.x-1, o.y) && !visited[o.x-1][o.y] &&  grid[o.x-1][o.y] == 1 ){
-                q.add(new O(o.x-1,o.y, o.layer + 1));
-                grid[o.x-1][o.y] = 2;
-                visited[o.x-1][o.y] = true;
+            for(int k = 0; k < x.length; k++){
+                int ro = cur.x + x[k];
+                int co = cur.y + y[k];
+                
+                if(inside(grid, ro, co) && grid[ro][co] == 1) {
+                    q.add(new O(ro, co,  cur.layer + 1));
+                    grid[ro][co] = 2;                    
+                }
+
+
             }
-            
-            //down
-            if( inside(grid,o.x+1, o.y) && !visited[o.x+1][o.y] && grid[o.x+1][o.y] == 1 ){
-                q.add(new O(o.x+1,o.y, o.layer + 1));
-                grid[o.x+1][o.y] = 2;
-                visited[o.x+1][o.y] = true;
-            }
-            
-            //left
-            if( inside(grid, o.x, o.y-1) && !visited[o.x][o.y-1] && grid[o.x][o.y-1] == 1 ){
-                q.add(new O(o.x,o.y-1, o.layer + 1));
-                grid[o.x][o.y-1] = 2;
-                visited[o.x][o.y-1] = true;
-            }
-            
-            
-            //right
-            if( inside(grid, o.x, o.y+1) && !visited[o.x][o.y+1] && grid[o.x][o.y+1] == 1 ){
-                q.add(new O(o.x,o.y+1, o.layer + 1));
-                grid[o.x][o.y+1] = 2;
-                visited[o.x][o.y+1] = true;
-            } 
             
         }
         
@@ -70,11 +52,11 @@ class Solution {
         return res;
     }
     
+    int[] x = {0,0,-1,1};
+    int[] y = {-1,1,0,0};
+    
     public boolean inside(int[][] grid, int i , int j){
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length)return  false;
         return true;
     }
 }
-
-//https://leetcode.com/problems/rotting-oranges/
-//https://leetcode.com/problems/rotting-oranges/discuss/238730/Java-DFS-with-comments(but-O(n2)
