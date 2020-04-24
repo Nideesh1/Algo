f419de2ff8a0c39f3b74c8fbbce269a0067ae6b2
Nideesh Terapalli <nterapalli1@gmail.com>
2020-04-23 20:35:39 -0700

    Update L_743.java

--- leetcode/L_743.java
+++ leetcode/L_743.java
@@ -1,36 +1,45 @@
 class Solution {
     public int networkDelayTime(int[][] times, int N, int K) {
-        Map<Integer, List<int[]>> graph = new HashMap();
-        for (int[] edge: times) {
-            if (!graph.containsKey(edge[0]))
-                graph.put(edge[0], new ArrayList<int[]>());
-            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
-        }
-        PriorityQueue<int[]> heap = new PriorityQueue<int[]>(
-                (info1, info2) -> info1[1] - info2[1]);
-        heap.offer(new int[]{K, 0});
-
-        //Key is node, val is dist from K
-        Map<Integer, Integer> dist = new HashMap();
-
-        while (!heap.isEmpty()) {
-            int[] info = heap.poll();
-            int d = info[1], node = info[0];
-            if (dist.containsKey(node)) continue;
-            dist.put(node, d);
-            if (graph.containsKey(node))
-                for (int[] edge: graph.get(node)) {
-                    int nei = edge[0], d2 = edge[1];
-                    if (!dist.containsKey(nei))
-                        heap.offer(new int[]{nei, d + d2});
+        Map<Integer,List<int[]>> weightAdj = new HashMap<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(
+            (int[] a, int[] b) -> a[1] - b[1]
+        );
+        
+        for(int[] edge : times){
+            List<int[]> nei = weightAdj.getOrDefault(edge[0], new ArrayList<>());
+            nei.add(new int[]{edge[1], edge[2]});
+            weightAdj.put(edge[0], nei);
+        }        
+        
+         int res = Integer.MIN_VALUE;
+        //map for our question's usecase. Key is node, value is distance 
+        //from origin node K. It also acts as a visited set
+        Map<Integer,Integer> dist = new HashMap<>();
+        pq.add(new int[]{K, 0});
+        
+        while(!pq.isEmpty()){
+            
+            int[] cur = pq.poll();
+            int node = cur[0];
+            int dis = cur[1];
+            
+            if(dist.containsKey(node)) continue;
+            dist.put(node, dis);
+            res = Math.max(res, dis);
+            if(weightAdj.containsKey(node))
+            for(int[] nei : weightAdj.get(node)){
+                
+                int neiNode = nei[0]; int neiDist = nei[1];
+                
+                if(!dist.containsKey(neiNode)){
+                    pq.add(new int[]{neiNode, neiDist + dis});
                 }
+            }
         }
+   
+        if(dist.size() != N) return -1;
 
-        if (dist.size() != N) return -1;
-        int ans = 0;
-        for (int cand: dist.values())
-            ans = Math.max(ans, cand);
-        return ans;
+        return res;
     }
 }
 
