e7529d96499a1507f50a633179c27e83e1c50634
Nideesh Terapalli <nterapalli1@gmail.com>
2020-04-26 22:31:33 -0700

    Update L_1423.java

--- leetcode/L_1423.java
+++ leetcode/L_1423.java
@@ -18,4 +18,43 @@ class Solution {
     }
 }
 
+//TLE
+class Solution {
+    Map<String,Integer> map = new HashMap<>();
+    int[][] cache;
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        cache = new int[n][n];
+        int res = dp(cardPoints, 0, n - 1, k);
+        return res;
+    }
+    
+    public int dp(int[] arr, int l, int r, int k){
+        if(cache[l][r] != 0) return cache[l][r];
+        //if(map.containsKey(l+"-"+r)) return map.get(l+"-"+r);
+        if(k == 1){
+            return Math.max(arr[l], arr[r]);
+        }
+
+        
+        int ls = dp(arr, l + 1, r, k - 1);
+        //map.put((l + 1) + "-"  + (r), ls);
+        cache[l+1][r] = ls;
+        
+        int rs = dp(arr, l, r - 1, k - 1);
+        //map.put(l + "-" + (r-1), rs);
+        cache[l][r-1] = rs;
+        
+        int left = arr[l] + ls;
+        int right = arr[r] + rs;
+        
+        int res = Math.max(left, right);
+        //map.put(l+"-"+r, res);
+        cache[l][r] = res;
+        return res;
+    }
+}
+
+
+//Good diagram. Different implementation not in Java
 //https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/discuss/597883/Javascript-and-C%2B%2B-solutions
