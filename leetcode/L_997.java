c164b1549b45f583ce8f3ae2fd59918f2f593d4e
Nideesh Terapalli <nterapalli1@gmail.com>
2020-05-10 19:13:15 -0700

    Update L_997.java

--- leetcode/L_997.java
+++ leetcode/L_997.java
@@ -1,27 +1,16 @@
 class Solution {
     public int findJudge(int N, int[][] trust) {
         if(trust.length == 0){return 1;}
-        Map<Integer,List<Integer>> town = new HashMap<>();
+        
+        Set<Integer> outgoing = new HashSet<>();
+        Map<Integer,Integer> indegree = new HashMap<>();
         for(int[] t : trust){
-            List<Integer> l = town.getOrDefault(t[0], new ArrayList<>());
-            l.add(t[1]);
-            town.put(t[0], l);
+            outgoing.add(t[0]);
+            indegree.put(t[1], indegree.getOrDefault(t[1], 0) +1);
         }
-        Map<Integer, Integer> trustCount = new HashMap<>();
-        for(List<Integer> l : town.values()){
-            for(Integer i : l){
-                int num = trustCount.getOrDefault(i,0) + 1;
-                trustCount.put(i,num);
-            }
-        }
-        System.out.println(trustCount);
-        for(Map.Entry<Integer,Integer> entry : trustCount.entrySet()){
-            if(entry.getValue() == N-1 && !town.containsKey(entry.getKey())){
-                return entry.getKey();
-            }
+        for(int in : indegree.keySet()){
+            if(indegree.get(in) == N - 1 && !outgoing.contains(in)) return in;
         }
         return -1;
     }
 }
-
-//https://leetcode.com/problems/find-the-town-judge/description/
