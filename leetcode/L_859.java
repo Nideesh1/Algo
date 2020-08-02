d1256458af57d150112c8695097e79a1aa8ba9c4
Nideesh Terapalli <nterapalli1@gmail.com>
2020-08-02 09:59:33 -0700

    Update L_859.java

--- leetcode/L_859.java
+++ leetcode/L_859.java
@@ -1,31 +1,27 @@
 class Solution {
     public boolean buddyStrings(String A, String B) {
-        if(A.length() != B.length())return false;
-        boolean Arepeat = false; boolean Brepeat = false;
-        Set<Character> Aset = new HashSet<>();
-        Set<Character> Bset = new HashSet<>();
+        if(A.length() != B.length()) return false;
+        Map<Character,Integer> mapA = new HashMap<>();
+        Map<Character,Integer> mapB = new HashMap<>();
         List<Integer> pos = new ArrayList<>();
         for(int i = 0; i < A.length(); i++){
             char a = A.charAt(i);
             char b = B.charAt(i);
-            if(Aset.contains(a)){Arepeat = true;}
-            if(Bset.contains(b)){Brepeat = true;}
-            if(A.charAt(i) != B.charAt(i)){
-                pos.add(i);
-            }
-            Aset.add(a); Bset.add(b);
+            
+            if(a != b) pos.add(i);
+            
+            mapA.put(a, mapA.getOrDefault(a, 0) + 1);
+            mapB.put(b, mapB.getOrDefault(b, 0) + 1);            
+        }
+        if(A.equals(B)){
+            for(Character c : mapA.keySet()) {if(mapA.get(c) > 1) return true;}
+            return false;
         }
         
-        //A's pos 0 has to equal B's pos 1
-        //A's pos 1 has to equal B's pos 0
-        if(pos.size() == 2){
-            if(A.charAt(pos.get(0)) == B.charAt(pos.get(1)) && A.charAt(pos.get(1)) == B.charAt(pos.get(0))){
-                return true;
-            }
-        }
-        
-        if(A.equals(B) && Arepeat && Brepeat){return true;}
-        return false;
+        if(pos.size() != 2) return false;
+        int posOne = pos.get(0);
+        int posTwo = pos.get(1);
+        return A.charAt(posOne) == B.charAt(posTwo) && A.charAt(posTwo) == B.charAt(posOne);
     }
 }
 
