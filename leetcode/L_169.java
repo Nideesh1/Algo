1565c326cdc0d1b27ce06bde5a5b3d1b6124ab03
Nideesh Terapalli <nterapalli1@gmail.com>
2020-08-11 22:48:21 -0700

    Update L_169.java

--- leetcode/L_169.java
+++ leetcode/L_169.java
@@ -1,52 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        return maj(nums, 0, nums.length-1);   
+        int n = nums.length;
+        return dfs(nums, 0, n -1);
     }
-    public int maj(int[] nums, int i, int j){
-        if(i == j){
-            return nums[i];
-        }
+    public int dfs(int[] nums, int i, int j){
+        if(i == j) return nums[i];
+        int m = (i + j)/2;
+        int left = dfs(nums, i,m);
+        int right = dfs(nums, m+1, j);
         
-        int mid = (i+j)/2;
+        if(left == right) return left;
         
-        int left = maj(nums, i, mid);
-        int right = maj(nums, mid+1, j);
-        
-        if(left == right){
-            return left;
-        }
-        
-        int leftInt = count(nums, left, i, j);
-        int rightInt = count(nums, right, i, j);
-        
-        return leftInt > rightInt ? left: right;
+        return count(nums, left, i , m) > count(nums, right, m + 1, j) ? left : right;
     }
-    public int count(int[] nums, int num, int i, int j){
-        int count = 0;
-        for(int k = i; k <= j; k++){
-            if(nums[k] == num){
-                count++;
-            }
+    
+    public int count(int[] ar, int c, int i, int j){
+        int res = 0;
+        for(int k = i; k <= j;k++){
+            if(ar[k] == c) res++;
         }
-        return count;
+        return res;
     }
 }
 
-
-public int majorityElement3(int[] nums) {
-    int count=0, ret = 0;
-    for (int num: nums) {
-        if (count==0)
-            ret = num;
-        if (num!=ret)
-            count--;
-        else
-            count++;
-    }
-    return ret;
-}
-
-
-
-//https://leetcode.com/problems/majority-element/description/
-//https://leetcode.com/problems/majority-element/discuss/51611/Java-solutions-(sorting-hashmap-moore-voting-bit-manipulation).
