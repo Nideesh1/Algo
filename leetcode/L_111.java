9cd315d12ebed98976ade41aa619d4db597d5def
Nideesh Terapalli <nterapalli1@gmail.com>
2020-08-13 20:29:18 -0700

    Update L_111.java

--- leetcode/L_111.java
+++ leetcode/L_111.java
@@ -27,5 +27,23 @@ class Solution {
         return 0;
     }
 }
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        
+        if(root.left == null && root.right == null ) return 1;
+        
+        int min = Integer.MAX_VALUE;
+        if(root.left != null){
+            min = Math.min(min, minDepth(root.left));
+        }
+
+        if(root.right != null){
+            min = Math.min(min, minDepth(root.right));
+        }
+        return min + 1;
+    }
+}
 
 //https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+//https://leetcode.com/problems/minimum-depth-of-binary-tree/solution/
