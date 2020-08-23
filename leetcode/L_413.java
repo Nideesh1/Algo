c20f95d57660730663612188731ba69e19839c5d
Nideesh Terapalli <nterapalli1@gmail.com>
2020-08-23 16:03:09 -0700

    Update L_413.java

--- leetcode/L_413.java
+++ leetcode/L_413.java
@@ -12,5 +12,25 @@ class Solution {
     }
 }
 
+public class Solution {
+    int sum = 0;
+    public int numberOfArithmeticSlices(int[] A) {
+        slices(A, A.length - 1);
+        return sum;
+    }
+    public int slices(int[] A, int i) {
+        if (i < 2)
+            return 0;
+        int ap = 0;
+        if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+            ap = 1 + slices(A, i - 1);
+            sum += ap;
+        } else {
+            slices(A, i - 1);
+        }
+        return ap;
+    }
+}
+
 //https://leetcode.com/problems/arithmetic-slices/
 //Solution Tab: https://leetcode.com/problems/arithmetic-slices/solution/
