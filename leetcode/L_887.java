c820fd641c416be5c4efcd325998a59e10d6576e
Nideesh Terapalli <nterapalli1@gmail.com>
2020-08-30 13:03:26 -0700

    Update L_887.java

--- leetcode/L_887.java
+++ leetcode/L_887.java
@@ -1,15 +1,58 @@
+//TLE
 class Solution {
     public int superEggDrop(int K, int N) {
-        int[][] dp = new int[N+1][K+1];
-        int m = 0;
-        while(dp[m][K] < N){
-            m++;
-            for(int k = 1; k <= K; k++){
-                dp[m][k] = dp[m-1][k-1] + dp[m-1][k] + 1;
-            } 
+        int eggs = K;
+        int floors = N;
+        return dfs(eggs, floors);
+    }
+    public int dfs(int eggs, int floors){
+        if(floors <= 1) return floors;
+        if(eggs == 1) return floors;
+        
+        int res = Integer.MAX_VALUE;
+        for(int i = 1; i <= floors; i++){
+                
+            int one = dfs(eggs - 1, i - 1); // below floor i
+            int two = dfs(eggs, floors - i);// above floor i
+            
+            res = Math.min(res, Math.max(one, two));
+            
         }
-        return m;
+        return res + 1;
     }
 }
 
-//https://leetcode.com/problems/super-egg-drop/discuss/158974/C%2B%2BJavaPython-2D-and-1D-DP-O(KlogN)
+class Solution {
+    public int superEggDrop(int K, int N) {
+        int[][] memo = new int[K + 1][N + 1];
+        return helper(K, N, memo);
+    }
+    private int helper(int K, int N, int[][] memo) {
+        if (N <= 1) {
+            return N;
+        }
+        if (K == 1) {
+            return N;
+        }
+        if (memo[K][N] > 0) {
+            return memo[K][N];
+        }
+        
+        int low = 1, high = N, result = N;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int left = helper(K - 1, mid - 1, memo);
+            int right = helper(K, N - mid, memo);
+            result = Math.min(result, Math.max(left, right) + 1);
+            if (left == right) {
+                break;
+            } else if (left < right) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        memo[K][N] = result;
+        return result;
+    }
+}
