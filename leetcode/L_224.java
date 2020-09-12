649eb8a9e543a69b75c4bfa0b7b067bd8553158f
Nideesh Terapalli <nterapalli1@gmail.com>
2020-09-11 20:24:31 -0700

    Update L_224.java

--- leetcode/L_224.java
+++ leetcode/L_224.java
@@ -1,43 +1,26 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stk = new Stack<>();
-        int operand = 0;
-        int sign = 1;
-        int result = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
+        Stack<Integer> stk = new Stack<>(); int res = 0, num = 0, sign = 1;
+        for(char c : s.toCharArray()){
             if(Character.isDigit(c)){
-                operand = operand * 10 + (int) (c - '0');
-            }
-            else if(c == '+'){
-                result += sign * operand;
-                sign = 1;
-                operand = 0;
-            }
-            else if(c == '-'){
-                result += sign * operand;
-                sign = -1;
-                operand = 0;
-            }
-            else if(c == '('){
-                stk.push(result);
-                stk.push(sign);
-                
-                result = 0;
-                sign = 1;
-            }
-            else if(c == ')'){
-                result += sign * operand;
-                
-                result *= stk.pop();
-                result += stk.pop();
-                
-                operand = 0;
+                num = 10 * num + (int) c - '0';
+            } else if(c == '+'){
+                res += num * sign;
+                sign = 1; num = 0;
+            } else if(c == '-'){
+                res += num * sign;
+                sign = -1; num = 0;
+            } else if(c == '('){
+                stk.push(res); stk.push(sign);
+                res = 0; sign = 1;
+            } else if(c == ')'){
+                res += num * sign;
+                num = 0; sign = 1;
+                res *= stk.pop(); res += stk.pop();
             }
         }
-        return result + (sign * operand);
+        if(num != 0) res += num * sign;
+        return res;
     }
 }
-
-//https://leetcode.com/problems/basic-calculator/solution/
+//https://leetcode.com/problems/maximum-product-subarray/discuss/48404/Accepted-Java-solution
