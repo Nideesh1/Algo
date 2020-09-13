81d258edeca8ce040fbf4e6947bdc2cf97fd0552
Nideesh Terapalli <nterapalli1@gmail.com>
2020-09-13 11:55:17 -0700

    Update L_1324.java

--- leetcode/L_1324.java
+++ leetcode/L_1324.java
@@ -1,39 +1,24 @@
 class Solution {
     public List<String> printVertically(String s) {
         List<String> res = new ArrayList<>();
-        TreeMap<Integer,String> map = new TreeMap<>();
-        String[] ar = s.split(" ");
-        int max = 0;
-        for(int i = 0; i < ar.length; i++){
-            ar[i] = ar[i].trim();
-            max = Math.max(max, ar[i].length());
+        String[] words = s.split(" ");
+        int max = 0;        
+        for(String w : words){
+            max = Math.max(max, w.length());
         }
-        for(int i = 0; i < ar.length; i++){
-            for(int j = 0; j < max; j++){
-                String del = "";
-                for(int k = 0; k < i; k++){
-                    del += " ";
-                }
-                
-                String v = map.getOrDefault(j, del);
-                v += j >= ar[i].length() ? " " : ar[i].charAt(j);
-                map.put(j, v);
+        for(int i = 0; i < max; i++){
+            StringBuilder sb = new StringBuilder();
+            for(String w : words){
+                sb.append(i < w.length() ? w.charAt(i) : " ");
             }
+            
+            while(sb.charAt(sb.length() - 1) == ' '){
+                sb.deleteCharAt(sb.length() - 1);
+            }
+            res.add(sb.toString());
         }
-        //System.out.println(map);
-        for(int i : map.keySet()){
-            res.add(trimEnd(map.get(i)));
-        }
-        //System.out.println(trimEnd("   T-  "));
         return res;
     }
-    
-    public String trimEnd(String str){
-        char[] ar = str.toCharArray();
-        int idx = ar.length - 1;
-        while(ar[idx] == ' '){
-            idx--;
-        }
-        return str.substring(0, idx + 1);
-    }
 }
+
+//https://leetcode.com/problems/print-words-vertically/discuss/484322/JavaPython-3-Straight-forward-codes-w-brief-explanation-and-analysis.
