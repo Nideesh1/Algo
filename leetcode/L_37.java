fdccefef7b385e3e0f5f889d3409f3db4f41256a
Nideesh Terapalli <nterapalli1@gmail.com>
2020-09-13 10:35:30 -0700

    Update L_37.java

--- leetcode/L_37.java
+++ leetcode/L_37.java
@@ -1,49 +1,54 @@
 class Solution {
+    Set<String> set = new HashSet<>();
     public void solveSudoku(char[][] board) {
-        if(board == null || board.length == 0) return;
-        dfs(board);
-    }
-    
-    public boolean dfs(char[][] board){
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                
-                if(board[i][j] == '.'){
-                    
-                    for(char c = '1'; c <= '9'; c++){
-                        
-                        if(isValid(board, i, j, c)){
-                            
-                            
-                            board[i][j] = c;
-                            
-                            if(dfs(board)){
-                                return true;
-                            }
-                            
-                            board[i][j] = '.';
-                        }
-                        
-                    }
-                    
-                    return false;
-                    
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] != '.'){
+                    addToSet(board[i][j], i, j);
                 }
-                
             }
         }
-        
+        dfs(board);
+    }
+    public boolean dfs(char[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    
+                    for(char k = '1'; k <= '9'; k++){
+                        
+                        if(!isValid(k, i, j)) continue;
+                        addToSet(k, i, j);
+                        board[i][j] = k;
+                        if(dfs(board)) return true;
+                        removeFromSet(k, i, j);
+                        board[i][j] = '.';
+                    }
+                    return false;
+                }
+            }
+        }
         return true;
     }
     
-    private boolean isValid(char[][] board, int row, int col, char num) {
-        int blkrow = (row / 3) * 3, blkcol = (col / 3) * 3; // Block no. is i/3, first element is i/3*3
-        for (int i = 0; i < 9; i++)
-            if (board[i][col] == num || board[row][i] == num || 
-                    board[blkrow + i / 3][blkcol + i % 3] == num)
-                return false;
+    public boolean isValid(char k, int i, int j){
+        if(set.contains(k + "row is " + i)) return false;
+        if(set.contains(k + "col is " + j)) return false;
+        if(set.contains(k + "block is " + i/3 + " " + j/3)) return false;
         return true;
     }
+    
+    public void addToSet(char k, int i, int j){
+        set.add(k + "row is " + i);
+        set.add(k + "col is " + j);
+        set.add(k + "block is " + i/3 + " " + j/3);
+    }
+    public void removeFromSet(char k, int i, int j){
+        set.remove(k + "row is " + i);
+        set.remove(k + "col is " + j);
+        set.remove(k + "block is " + i/3 + " " + j/3);
+    }    
+    
 }
 
-//https://leetcode.com/problems/sudoku-solver/discuss/15752/Straight-Forward-Java-Solution-Using-Backtracking
+//https://leetcode.com/problems/sudoku-solver/discuss/693002/Java-Easy-to-understandread-code
