90d1e2576736415c025e3a6d0b4adae742ac26a2
Nideesh Terapalli <nterapalli1@gmail.com>
2020-09-22 19:41:07 -0700

    Update L_54.java

--- leetcode/L_54.java
+++ leetcode/L_54.java
@@ -1,38 +1,27 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> l = new ArrayList<>();
-        int m = matrix.length;
-        if(m == 0) return l;
-        int n = matrix[0].length;
-
-        int r1 = 0; 
-        int r2 = m-1;
-        int c1 = 0;
-        int c2 = n-1;
-        while( r1 <= r2 && c1 <= c2){
-            for(int i = c1; i <= c2; i++){
-                l.add(matrix[r1][i]);
-            }
-            for(int i = r1 + 1; i <= r2; i++){
-                l.add(matrix[i][c2]);
-            }         
+        List<Integer> res = new LinkedList<>(); 
+        if (matrix == null || matrix.length == 0) return res;
+        int n = matrix.length, m = matrix[0].length;
+        int up = 0,  down = n - 1;
+        int left = 0, right = m - 1;
+        while (res.size() < n * m) {
+            for (int j = left; j <= right && res.size() < n * m; j++)
+                res.add(matrix[up][j]);
             
-            
-            if(r1 < r2 && c1 < c2){
-                for(int i = c2-1; i > c1; i--){
-                    l.add(matrix[r2][i]);
-                }            
-                for(int i = r2; i > r1; i--){
-                    l.add(matrix[i][c1]);
-                }                
-            }
-
-            
-            r1++; r2--;
-            c1++; c2--;
+            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
+                res.add(matrix[i][right]);
+                     
+            for (int j = right; j >= left && res.size() < n * m; j--)
+                res.add(matrix[down][j]);
+                        
+            for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--) 
+                res.add(matrix[i][left]);
+                
+            left++; right--; up++; down--; 
         }
-        return l;
+        return res;
     }
 }
 
-//https://leetcode.com/problems/spiral-matrix/solution/
+//https://leetcode.com/problems/spiral-matrix/discuss/20599/Super-Simple-and-Easy-to-Understand-Solution/185257
