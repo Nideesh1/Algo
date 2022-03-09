8deff8f538a861869cad97fcb3e13ad1a455e829
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-09 13:44:35 -0500

    Update L_3.java

--- leetcode/L_3.java
+++ leetcode/L_3.java
@@ -1,28 +1,28 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() == 0) return 0;
-        int[] ar = new int[256];
-        int size = Integer.MIN_VALUE;
-        int start = 0;
-        for(int j = 0; j < s.length(); j++){
-            ar[s.charAt(j)]++;
-            while(!valid(ar) && start < j){
-                ar[s.charAt(start)]--;
-                start++;
-            }
-            if(j - start + 1 > size){
-                size = j - start + 1;
+        int left = 0; // start of window
+        int right = 0; // end of window (initially the same as start so we can grab the first char)
+        int result = 0; // var to store the length of the longest substring we have found so far
+        HashSet<Character> chars = new HashSet(); // hashset to track the chars in the current substring
+		// the right hand side of the window must not go over the bounds of the string
+        while(right < s.length()) {
+            // use the right hand side of the window to get the char, it is initialised to 0 so will pick the first character first
+            if(!chars.contains(s.charAt(right))) {
+                chars.add(s.charAt(right));
+				// if the current set of unique chars is greater in size than the result, that's our longest substring
+                result = Math.max(result, chars.size());
+				// increment right to increase the window size and grab a new char
+                right++;
+            } else {
+			// once we encounter a char which is already in the hashset we need create a new window starting from left + 1, and reset right to equal left to initialise a new window
+                left ++;
+                right = left;
+				// reset our tracker
+                chars.clear();
             }
         }
-        return size;
-    }
-    
-    public boolean valid(int[] ar){
-        for(int i : ar){
-            if (i > 1) return false;
-        }
-        return true;
+        return result;
     }
 }
 
-//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/1629847/Java-Simple-Sliding-Window-Technique-With-Comments
