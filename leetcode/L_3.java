253e846e08603c748c6742848d10c8eda87cbda8
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-09 14:24:13 -0500

    Update L_3.java

--- leetcode/L_3.java
+++ leetcode/L_3.java
@@ -1,27 +1,22 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0; // start of window
-        int right = 0; // end of window (initially the same as start so we can grab the first char)
-        int result = 0; // var to store the length of the longest substring we have found so far
-        HashSet<Character> chars = new HashSet(); // hashset to track the chars in the current substring
-		// the right hand side of the window must not go over the bounds of the string
-        while(right < s.length()) {
-            // use the right hand side of the window to get the char, it is initialised to 0 so will pick the first character first
-            if(!chars.contains(s.charAt(right))) {
-                chars.add(s.charAt(right));
-				// if the current set of unique chars is greater in size than the result, that's our longest substring
-                result = Math.max(result, chars.size());
-				// increment right to increase the window size and grab a new char
-                right++;
-            } else {
-			// once we encounter a char which is already in the hashset we need create a new window starting from left + 1, and reset right to equal left to initialise a new window
-                left ++;
-                right = left;
-				// reset our tracker
-                chars.clear();
+        Map<Character,Integer> map = new HashMap<>();
+        int l = 0, r = 0, res = 0, repeats = 0;
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            r++;
+            map.put(c, map.getOrDefault(c,0) + 1);
+            if (map.get(c) > 1) repeats++;
+            
+            while (repeats > 0) {
+                char left = s.charAt(l);
+                if (map.get(left) > 1) repeats--;
+                map.put(left, map.get(left) - 1);
+                l++;
             }
+            res = Math.max(res, r -l);
         }
-        return result;
+        return res;
     }
 }
 
