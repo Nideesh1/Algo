4b82522281b5c892bc3ed1aefca851a70699a9b1
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-27 12:40:40 -0400

    Update L_73.java

--- leetcode/L_73.java
+++ leetcode/L_73.java
@@ -1,47 +1,37 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean firstColZero = false;
-        
-        //check if first column needs to be zero
-        //if i,j is zero, then set the first row and col in that combo to 0
-        for(int i = 0; i < m; i++){           
-            if(matrix[i][0] == 0){
-                firstColZero = true;
-            }          
-            for(int j = 1; j < n; j++){               
-                if(matrix[i][j] == 0){
+        boolean fr = false, fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
-                }             
+                }
             }
         }
-        
-        
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
         
-        
-        if(matrix[0][0] == 0){
-            for(int j = 0; j < n ; j++){
+        if (fr) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 matrix[0][j] = 0;
             }
         }
-        
-        if(firstColZero){
-            for(int i = 0; i < m; i++){
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
                 matrix[i][0] = 0;
             }
         }
         
-        
+
     }
 }
 
-//https://leetcode.com/problems/set-matrix-zeroes/
+//https://leetcode.com/problems/set-matrix-zeroes/discuss/26008/My-AC-java-O(1)-solution-(easy-to-read)
