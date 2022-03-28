45d49cf793bb8276f74f1a452379c1fad7198b9e
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-28 16:49:08 -0400

    Update L_50.java

--- leetcode/L_50.java
+++ leetcode/L_50.java
@@ -1,19 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        double res  = 1;
-        long absN = Math.abs( (long) n);
+        return dfs(x,n);
+    }
+    public double dfs(double x, int n) {
+        if (n == 0) return 1; 
+        if (x == 0 || x == 1 || n == 1) return x;
         
-        while(absN > 0){
-            if((absN & 1) == 1){
-                res = res * x;
-            }
-            absN = absN >> 1;
-            x = x * x;
-        } 
-        return n < 0 ? 1/res : res;
+        if (n < 0) {
+            return (1/x) * dfs(1/x, -(n+1));
+        }
+        
+        return n % 2 == 0 ? dfs(x*x, n /2) : x * dfs(x*x, n / 2);
     }
 }
-
-//N = 9 = 2^3 + 2^0 = 1001 in binary. Then:
-//x^9 = x^(2^3) * x^(2^0)
-//https://leetcode.com/problems/powx-n/discuss/19563/Iterative-Log(N)-solution-with-Clear-Explanation
+//https://leetcode.com/problems/powx-n/discuss/1515322/Java-or-TC%3A-O(logN)-or-Optimized-Recursive-and-Iterative-(handles-overflow)-solutions
