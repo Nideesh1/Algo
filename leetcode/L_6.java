1b1a3147eede08eed5dd7ffb958a4b90ba1b2926
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-28 08:11:40 -0400

    Update L_6.java

--- leetcode/L_6.java
+++ leetcode/L_6.java
@@ -1,32 +1,23 @@
 class Solution {
-    public String convert(String s, int nRows) {
-        char[] c = s.toCharArray();
-        int len = c.length;
+    public String convert(String s, int numRows) {
         
-        StringBuilder[] sb = new StringBuilder[nRows];
-        for(int i = 0; i < sb.length; i++){
-            sb[i] = new StringBuilder();
+        if (numRows == 1) return s;
+        
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+            list.add(new StringBuilder());
         }
         
-        int i = 0;
-        while(i < len){
-            
-            for(int idx = 0; idx < nRows && i < len; idx++){
-                sb[idx].append(c[i++]);
-            }
-            
-            for(int idx = nRows - 2; idx >= 1 && i < len; idx--){
-                sb[idx].append(c[i++]);
-            }
-            
-        }
+        int curRow = 0;
+        boolean down = false;
         
-        for(int idx = 1; idx < sb.length; idx++){
-            sb[0].append(sb[idx]);
+        for (char c : s.toCharArray()) {
+            list.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) down = !down;
+            curRow += down ? 1 : -1;
         }
-        
-        return sb[0].toString();
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder sb : list) res.append(sb);
+        return res.toString();
     }
 }
-
-//https://leetcode.com/problems/zigzag-conversion/discuss/3403/Easy-to-understand-Java-solution
