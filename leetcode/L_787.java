1f2f2d526542c2f7ce11da57ed159219e8e72c86
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-28 15:02:19 -0400

    Update L_787.java

--- leetcode/L_787.java
+++ leetcode/L_787.java
@@ -41,3 +41,36 @@ class Solution {
 }
 
 //https://leetcode.com/problems/cheapest-flights-within-k-stops/discuss/115541/JavaPython-Priority-Queue-Solution
+
+
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer,List<int[]>> map = new HashMap<>();
+        for (int[] f : flights) {
+            int u = f[0], v = f[1], w = f[2];
+            List<int[]> list = map.getOrDefault(u, new ArrayList<>());
+            list.add(new int[]{v, w, 0});
+            map.put(u, list);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.add(new int[]{src, 0, -1});
+        int[] stops = new int[n];
+        Arrays.fill(stops, Integer.MAX_VALUE);
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int node = cur[0], dist = cur[1], stop = cur[2];
+            if (stop > k || stop > stops[node]) continue;
+            stops[node] = stop;
+            if (node == dst) return dist;
+               if (map.containsKey(node)) {
+                for (int[] nei : map.get(node)) {
+                    int neiNode = nei[0];
+                    int neiDist = nei[1];
+                    int neiStop = nei[2];
+                    pq.add(new int[]{neiNode, neiDist + dist, stop + 1});
+                }
+            }
+        }
+        return -1;
+    }
+}
