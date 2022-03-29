899353e4fde3bfd7bc0b4e329a37d068ada7e1fc
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-28 21:45:52 -0400

    Update L_445.java

--- leetcode/L_445.java
+++ leetcode/L_445.java
@@ -38,4 +38,67 @@ class Solution {
     }
 }
 
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // We will use sizes to understand which list's nodes should be frozen for a while.
+        int s1 = size(l1);
+        int s2 = size(l2);
+        ListNode resHead = null;
+        ListNode n = null;
+        while (l1 != null || l2 != null) {
+            int v1 = 0;
+            int v2 = 0;
+            if (s1 >= s2) {
+                v1 = l1 != null ? l1.val : 0;
+                l1 = l1.next;
+                s1--;
+            }
+            // Comparing with s1 + 1 since s1 might be decremented previously
+            if (s2 >= s1 + 1) {
+                v2 = l2 != null ? l2.val : 0;
+                l2 = l2.next;
+                s2--;
+            }
+            // Creating the resulting list in the reversed order.
+            n = new ListNode(v1 + v2);
+            n.next = resHead;
+            resHead = n;
+        }
+        int carry = 0;
+        resHead = null;
+        // Now, let's perform the normalization.
+        while (n != null) {
+            n.val += carry;
+            if (n.val >= 10) {
+                n.val = n.val % 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            ListNode buf = n.next;
+            n.next = resHead;
+            resHead = n;
+            n = buf;
+        }
+        if (carry > 0) {
+            n = new ListNode(1);
+            n.next = resHead;
+            resHead = n;
+        }
+        return resHead;
+    }
+
+    private int size(ListNode l) {
+        int s = 0;
+        while (l != null) {
+            l = l.next;
+            s++;
+        }
+        return s;
+    }
+
+}
+
+//https://leetcode.com/problems/add-two-numbers-ii/discuss/687339/Java-O(N)-solution-with-follow-up-question-no-recursion-no-stacks
+
 //https://leetcode.com/problems/add-two-numbers-ii/discuss/92623/Easy-O(n)-Java-Solution-using-Stack
