da101dc0742bf01d328f8b43fbb07f0f145689ce
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-29 21:11:03 -0400

    Update L_4.java

--- leetcode/L_4.java
+++ leetcode/L_4.java
@@ -1,45 +1,42 @@
-class Solution {
+public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
+        // Deal with invalid corner case. 
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) return 0.0;
         
-        int len = m + n;
-
-        if(len % 2 == 0){
-            int one = dfs(nums1, 0, nums2, 0, len/2);
-            int two = dfs(nums1, 0, nums2, 0, len/2 + 1);
-            
-            return (double) (one + two)/2;
-        }else{
-            return dfs(nums1, 0, nums2, 0, len/2 + 1);
-        }
-    }
-    public int dfs(int[] a, int aStart, int[] b, int bStart, int k){
-        if(aStart >= a.length){
-            return b[bStart + k - 1];
-        }
-        if(bStart >= b.length){
-            return a[aStart + k - 1];
-        }
-        if(k == 1){
-            return Math.min(a[aStart], b[bStart]);
-        }
+        int m = nums1.length, n = nums2.length;
+        int l = (m + n + 1) / 2; //left half of the combined median
+        int r = (m + n + 2) / 2; //right half of the combined median
         
-        int amid = aStart + k/2 - 1;
-        int bmid = bStart + k/2 - 1;
-        
-        int aval = amid >= a.length ? Integer.MAX_VALUE : a[amid];
-        int bval = bmid >= b.length ? Integer.MAX_VALUE : b[bmid];
-        
-        if(aval <= bval){
-            return dfs(a, amid + 1, b, bStart, k - k / 2);
-        }else{
-            return dfs(a, aStart, b, bmid + 1, k - k/2);
-        }
+        // If the nums1.length + nums2.length is odd, the 2 function will return the same number
+        // Else if nums1.length + nums2.length is even, the 2 function will return the left number and right number that make up a median
+        return (getKth(nums1, 0, nums2, 0, l) + getKth(nums1, 0, nums2, 0, r)) / 2.0;
     }
     
+    private double getKth(int[] nums1, int start1, int[] nums2, int start2, int k) {
+        // This function finds the Kth element in nums1 + nums2
+        
+        // If nums1 is exhausted, return kth number in nums2
+        if (start1 > nums1.length - 1) return nums2[start2 + k - 1];
+        
+        // If nums2 is exhausted, return kth number in nums1
+        if (start2 > nums2.length - 1) return nums1[start1 + k - 1];
+        
+        // If k == 1, return the first number
+        // Since nums1 and nums2 is sorted, the smaller one among the start point of nums1 and nums2 is the first one
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+        
+        int mid1 = Integer.MAX_VALUE;
+        int mid2 = Integer.MAX_VALUE;
+        if (start1 + k / 2 - 1 < nums1.length) mid1 = nums1[start1 + k / 2 - 1];
+        if (start2 + k / 2 - 1 < nums2.length) mid2 = nums2[start2 + k / 2 - 1];
+        
+        // Throw away half of the array from nums1 or nums2. And cut k in half
+        if (mid1 < mid2) {
+            return getKth(nums1, start1 + k / 2, nums2, start2, k - k / 2); //nums1.right + nums2
+        } else {
+            return getKth(nums1, start1, nums2, start2 + k / 2, k - k / 2); //nums1 + nums2.right
+        }
+    }
 }
 
 //https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2496/Concise-JAVA-solution-based-on-Binary-Search
-// if (aMid < bMid) Keep [aRight + bLeft]    
-// else Keep [bRight + aLeft]
