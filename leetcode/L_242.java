ac79ca10a4877abf48d375bba72e629ebce01521
Nideesh Terapalli <nterapalli1@gmail.com>
2022-03-31 12:10:02 -0400

    Update L_242.java

--- leetcode/L_242.java
+++ leetcode/L_242.java
@@ -28,4 +28,34 @@ class Solution {
     }
 }
 
+
+class Solution {
+public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+    Map<Integer, Integer> sm = computeFrequencyMap(s);
+    Map<Integer, Integer> tm = computeFrequencyMap(t);
+    System.out.println(sm);
+    return sm.equals(tm);
+}
+
+private Map<Integer, Integer> computeFrequencyMap(String s) {
+    Map<Integer, Integer> sm = new HashMap<>();
+    for (int i = 0; i < s.length(); ++i) {
+        char cs = s.charAt(i);
+        int cp;
+        if (i < s.length() - 1 && Character.isHighSurrogate(cs)) {
+            char cn = s.charAt(i + 1);
+            cp = Character.isLowSurrogate(cn) ? Character.toCodePoint(cs, cn) : cs;
+        } else {
+            cp = cs;
+        }
+        i = cp == cs ? i : i + 1;
+        sm.put(cp, sm.getOrDefault(cp, 0) + 1);
+    }
+    return sm;
+}
+}
+
 //https://leetcode.com/problems/valid-anagram/description/
