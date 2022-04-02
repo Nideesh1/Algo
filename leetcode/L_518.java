415fbe6510abe908356799e35b9702b220f4d7b7
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-02 15:58:08 -0400

    Update L_518.java

--- leetcode/L_518.java
+++ leetcode/L_518.java
@@ -1,15 +1,32 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
-        for(int coin : coins){
-            for(int x = coin; x < amount + 1; x++){
-                dp[x] += dp[x - coin];
-        //        System.out.println(Arrays.toString(dp));
+        int[] dp = new int[amount+1];
+        dp[0] =1;
+        for (int c : coins) {
+            for (int p = c; p < dp.length; p++) {
+                dp[p] += dp[p - c]; 
             }
         }
-
         return dp[amount];
+    }
+}
+
+class Solution {
+    Map<String,Integer> map = new HashMap<>();
+    public int change(int amount, int[] coins) {
+        return dfs(amount, coins, 0);
+    }
+    public int dfs(int amount, int[] coins, int start) {
+        if (amount < 0) return 0;
+        if (amount == 0) return 1;
+        String key = amount + "," + start;
+        if (map.containsKey(key)) return map.get(key);
+        int res = 0;
+        for (int i = start; i < coins.length; i++) {
+            res += dfs(amount - coins[i], coins, i);
+        }
+        map.put(key, res);
+        return res;
     }
 }
 
