6f1c83d569dcb693058771127a3648b53faf6db3
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-02 21:21:25 -0400

    Update L_1656.java

--- leetcode/L_1656.java
+++ leetcode/L_1656.java
@@ -16,3 +16,25 @@ class OrderedStream {
         return res;
     }
 }
+
+class OrderedStream {
+    //String[] ar;
+    List<String> ar;
+    int p;
+    public OrderedStream(int n) {
+        //ar = new String[n+1];
+        ar = Arrays.asList(new String[n+1]);
+        p = 1;
+    }
+    
+    public List<String> insert(int idKey, String value) {
+        List<String> res = new ArrayList<>();
+        //ar[idKey] = value; 
+        ar.set(idKey, value);
+        if (idKey > p) return res;
+        while (p < ar.size() && ar.get(p) != null) {
+            res.add(ar.get(p++));
+        }
+        return res;
+    }
+}
