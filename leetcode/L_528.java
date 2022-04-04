ba8734e13a1d750fdd7d41706ea8e05d5e20acaa
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-03 21:38:27 -0400

    Update L_528.java

--- leetcode/L_528.java
+++ leetcode/L_528.java
@@ -1,31 +1,37 @@
 class Solution {
-    List<Integer> psum = new ArrayList<>();
-    int tot = 0;
+    int[] prefix;
+    int tot;
     Random rand = new Random();
     public Solution(int[] w) {
-        for(int x : w){
-            tot += x;
-            psum.add(tot);
+        prefix = new int[w.length];
+        tot = 0;
+        prefix[0] = w[0];
+        for (int i = 1; i < w.length; i++) {
+            prefix[i] += prefix[i-1] + w[i];
         }
+        tot = prefix[w.length - 1];
     }
     
     public int pickIndex() {
-        int targ = rand.nextInt(tot);
-        
-        int lo = 0;
-        int hi = psum.size() - 1;
-        while(lo != hi){
-            int mid = lo + (hi - lo)/2;
-            if(targ < psum.get(mid)){
-                hi = mid;
-            }else{
-                lo = mid + 1;
+        int target = rand.nextInt(tot);
+        int l = 0, r = prefix.length - 1;
+        while (l < r) {
+            int m = (l+r)/2;
+            if (target < prefix[m]) {
+                r = m;
+            } else {
+                l = m + 1;
             }
         }
-        
-        return lo;
+        return l;
     }
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
 
 /**
  * Your Solution object will be instantiated and called as such:
