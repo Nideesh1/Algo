7538d1e7938a8a47d1540fd8c8f730c0678acbb8
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-05 17:00:50 -0400

    Update L_1029.java

--- leetcode/L_1029.java
+++ leetcode/L_1029.java
@@ -1,38 +1,18 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        Comparator co = new Comparator<int[]>() {
-            public int compare(int[] a, int[]b){
-                return Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1]);
+        Comparator<int[]> co = new Comparator<>(){
+            public int compare (int[] one, int[] two) {
+                return (one[0] - one[1]) - (two[0] - two[1]);
             }
         };
-        int n  = costs.length/2;
-        int res = 0;
-        int countA = 0; int countB = 0;
+        
         Arrays.sort(costs, co);
         
-        for(int i = 0; i < costs.length; i++){
-            for(int j = 0; j < costs[0].length; j++){
-                System.out.print(costs[i][j] + " ");
-            }
-            System.out.println();
+        int res = 0, n = costs.length / 2;
+        for (int i = 0; i < n; i++) {
+            res += costs[i][0] + costs[i + n][1];
         }
         
-        for(int i = 0; i < costs.length; i++){
-            if(countA < n && countB < n){
-                if(costs[i][0] < costs[i][1]){
-                    countA++; res += costs[i][0];
-                }
-                if(costs[i][0] >= costs[i][1]){
-                    countB++; res += costs[i][1];
-                }                
-            }
-            else if(countA < n){
-                countA++; res += costs[i][0];
-            }
-            else if(countB < n){
-                countB++; res += costs[i][1];
-            }
-        }
         
         return res;
     }
