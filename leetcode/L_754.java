7742ab7b6ec93043713c6fd359f3bdaef7aad992
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-04 21:27:25 -0400

    Update L_754.java

--- leetcode/L_754.java
+++ leetcode/L_754.java
@@ -1,24 +1,16 @@
 class Solution {
     public int reachNumber(int target) {
         target = Math.abs(target);
-        int s = 0; int i = 1;
-        while(s < target){
-            s+= i;
-            i++;
+        int step = 0;
+        int sum = 0;
+        while (sum < target) {
+            step++;
+            sum += step;
         }
-        i--;
-        int del = s - target;
-        if(del % 2 == 0) return i;
- 
-        // System.out.println(s);
-        // System.out.println(i);
-        // System.out.println(del);
-        
-        while( del % 2 != 0){
-            del += ++i;
+        while ((target - sum ) % 2 != 0) {
+            step++;
+            sum += step;
         }
-        return i;
+        return step;
     }
 }
-
-//https://leetcode.com/problems/reach-a-number/
