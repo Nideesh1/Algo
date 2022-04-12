12e11b356e8ca275e0105fb4dabebf2a2f2f28f3
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-12 14:16:57 -0400

    Create L_1239.java

---
+++ leetcode/L_1239.java
@@ -0,0 +1,32 @@
+class Solution {
+    int res = 0;
+    public int maxLength(List<String> arr) {
+        dfs(new StringBuilder(), arr, 0);
+        return res;
+    }
+    public void dfs(StringBuilder sb, List<String> arr, int p) {
+        if (p > arr.size()) return;
+        res = Math.max(sb.length(), res);
+        //System.out.println(sb);
+        for (int i = p; i < arr.size(); i++) {
+            if (duplicate(sb, arr.get(i))) continue;
+            //int len = sb.length();
+            sb.append(arr.get(i));
+            dfs(sb, arr, i + 1);
+            //sb.setLength(len);
+            sb.delete(sb.length() - arr.get(i).length(), sb.length());
+        }
+    }
+    
+    
+    public boolean duplicate (StringBuilder sb, String str) {
+        Set<Character> temp = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            temp.add(c);
+            if(sb.indexOf(c + "") != -1) return true;
+        }
+        //System.out.println(temp);
+        return temp.size() != str.length() ? true : false;
+    }
+}
