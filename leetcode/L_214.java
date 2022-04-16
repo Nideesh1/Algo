bfa58b96fbd0467fa76e00f2384dbb3e1c9ec5c5
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-16 17:36:14 -0400

    Update L_214.java

--- leetcode/L_214.java
+++ leetcode/L_214.java
@@ -1,16 +1,20 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        int n = s.length();
-        int i = 0;
-        for(int j = n - 1; j >= 0; j--){
-            if(s.charAt(i) == s.charAt(j)){
+        // for finding the answer by appending the end, use StringBuider reverse3
+        return dfs(s);
+    }
+    public String dfs (String s) {
+        int i = 0, j = s.length() - 1;
+        while (j >= 0) {
+            if (s.charAt(i) == s.charAt(j)) {
                 i++;
             }
+            j--;
         }
-        if(i == n)return s;
+        if (i == s.length()) return s;
         String rem = s.substring(i);
         String rev = new StringBuilder(rem).reverse().toString();
-        return rev + shortestPalindrome(s.substring(0,i)) + rem;
+        return rev + dfs(s.substring(0,i)) + rem;
     }
 }
 //https://leetcode.com/problems/shortest-palindrome/solution/
