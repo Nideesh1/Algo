bff408333a5a8458f87584fdbfe1613ea1871fd9
Nideesh Terapalli <nterapalli1@gmail.com>
2022-04-17 19:39:18 -0400

    Update L_611.java

--- leetcode/L_611.java
+++ leetcode/L_611.java
@@ -1,25 +1,20 @@
 class Solution {
     public int triangleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
         int res = 0;
-        for(int i = n - 1; i >= 2; i--){
-            
-            int lo = 0;
-            int hi = i - 1;
-            
-            while(lo < hi){
-                
-                int sum = nums[lo] + nums[hi];
-                if(sum > nums[i]){
-                    res += hi - lo;
-                    hi--;
-                }else{
-                    lo++;
+        Arrays.sort(nums);
+        for (int i = 2; i < nums.length; i++) {
+            int l = 0;
+            int r = i-1;
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum > nums[i]) {
+                    // for a fixed i and r. We will always have valid triangles by moving the l to the right
+                    res += r - l;
+                    r--;
+                } else {
+                    l++;
                 }
-                
             }
-            
         }
         return res;
     }
