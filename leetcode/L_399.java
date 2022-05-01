acb7241a79c1640161c2bf0bbc4181fb60c08bb8
Nideesh Terapalli <nterapalli1@gmail.com>
2022-05-01 14:42:04 -0400

    Update L_399.java

--- leetcode/L_399.java
+++ leetcode/L_399.java
@@ -1,56 +1,55 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        double[] res = new double[queries.size()];
         Map<String,Map<String,Double>> map = new HashMap<>();
+        double[] res = new double[queries.size()];
         for (int i = 0; i < equations.size(); i++) {
             List<String> eq = equations.get(i);
             String u = eq.get(0), v = eq.get(1);
             double val = values[i];
             
-            Map<String,Double> one = map.getOrDefault(u, new HashMap<>());
-            one.put(v, val);
-            map.put(u, one);
-
-            Map<String,Double> two = map.getOrDefault(v, new HashMap<>());
-            two.put(u, 1/val);
-            map.put(v, two);        
-        
-        } 
+            Map<String,Double> m1 = map.getOrDefault(u, new HashMap<>());
+            m1.put(v, val);
+            map.put(u, m1);
+            
+            Map<String,Double> m2 = map.getOrDefault(v, new HashMap<>());
+            m2.put(u, 1 / val);
+            map.put(v, m2);            
+        }
+//        System.out.println(map);
         for (int i = 0; i < queries.size(); i++) {
             List<String> query = queries.get(i);
             String dividend = query.get(0), divisor = query.get(1);
             if (!map.containsKey(dividend) || !map.containsKey(divisor)) {
                 res[i] = -1;
-            } else if (divisor == dividend) {
+            } else if (dividend.equals(divisor)) {
                 res[i] = 1;
             } else {
                 Set<String> set = new HashSet<>();
-                res[i] = dfs (map, dividend, divisor, 1, set);
+                res[i] = dfs(map, set, dividend, divisor, 1.0);
             }
         }
-        //System.out.println(map);
         return res;
     }
-    public double dfs(Map<String,Map<String,Double>> map, String cur, String target, double mul, Set<String> set) {
-        if (!map.containsKey(cur)) return -1;
+    public double dfs(Map<String,Map<String,Double>> map, Set<String> set, String cur, String divisor, double val) {
         double res = -1;
         set.add(cur);
         
-        Map<String,Double> neis = map.get(cur);
-        if (neis.containsKey(target)) {
-            res = mul * neis.get(target);
+        if (divisor.equals(cur)) {
+            res = val;
         } else {
-            for (String nei : neis.keySet()) {
-                if (set.contains(nei)) continue;
-                res = dfs(map, nei, target, mul * neis.get(nei), set);
-                //we found it, stop recursion
-                if (res != -1) break;
+            for (String key : map.get(cur).keySet()) {
+                    if (set.contains(key)) {
+                        continue;
+                    }
+                    double temp = dfs(map, set, key, divisor, val * (map.get(cur).get(key)));
+                    if (temp != -1) {
+                        res = temp;
+                        break;
+                    }
             }
         }
-        
         
         set.remove(cur);
         return res;
     }
-    
 }
